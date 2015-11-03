095503336f79e5f27d064c51bf6953837c63d1cb
Igor Zubkov <igor.zubkov@gmail.com>
2015-11-03 22:28:31 +0200

    More rspec examples

--- spec/eve_online/base_spec.rb
+++ spec/eve_online/base_spec.rb
@@ -3,6 +3,22 @@ require 'nori'
 require 'eve_online/base'
 
 describe EveOnline::Base do
+  describe '#initialize' do
+    let(:key_id) { '123key' }
+
+    let(:v_code) { '123v_code' }
+
+    let(:parser) { Nori.new }
+
+    subject { described_class.new(key_id, v_code, parser) }
+
+    its(:key_id) { should eq(key_id) }
+
+    its(:v_code) { should eq(v_code) }
+
+    its(:parser) { should eq(parser) }
+  end
+
   describe '#url' do
     specify { expect { subject.url }.to raise_error(NotImplementedError) }
   end
@@ -27,7 +43,7 @@ describe EveOnline::Base do
   end
 
   describe '#response' do
-    let(:parser) { double('parser') }
+    let(:parser) { double }
 
     let(:content) { 'some xml content' }
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'eveonline'
+require 'eve_online'
 
 # webmock
 require 'webmock/rspec'
 
+# rspec-its
+require 'rspec/its'
