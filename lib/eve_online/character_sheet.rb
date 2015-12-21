662627d047a55c377ffe7277ede9fd92bddb72b0
Igor Zubkov <igor.zubkov@gmail.com>
2015-12-21 22:52:32 +0200

    Add dummy EveOnline::Characters::CharacterSheet

--- lib/eve_online/character_sheet.rb
+++
@@ -1,6 +0,0 @@
-# TODO: finish this
-module EveOnline
-  # http://wiki.eve-id.net/APIv2_Char_CharacterSheet_XML
-  class CharacterSheet < Base
-  end
-end
---
+++ spec/eve_online/characters/character_sheet_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe EveOnline::Characters::CharacterSheet do
+  let(:key_id) { 123 }
+
+  let(:v_code) { 'abc' }
+
+  let(:character_id) { 12345678 }
+
+  subject { described_class.new(key_id, v_code, character_id) }
+
+  specify { expect(described_class::API_ENDPOINT).to eq('https://api.eveonline.com/char/CharacterSheet.xml.aspx') }
+
+  describe '#initialize' do
+    its(:key_id) { should eq(key_id) }
+
+    its(:v_code) { should eq(v_code) }
+
+    its(:character_id) { should eq(character_id) }
+  end
+
+  describe '#url' do
+    specify do
+      expect(subject.url).to eq("#{ described_class::API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }")
+    end
+  end
+end
