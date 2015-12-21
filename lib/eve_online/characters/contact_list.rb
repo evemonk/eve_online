2ef101f77c45693936207ace12cdd6df607f5389
Igor Zubkov <igor.zubkov@gmail.com>
2015-12-21 23:10:19 +0200

    Add dummy EveOnline::Characters::ContactList

---
+++ lib/eve_online/characters/contact_list.rb
@@ -0,0 +1,21 @@
+module EveOnline
+  module Characters
+    # http://wiki.eve-id.net/APIv2_Char_ContactList_XML
+    class ContactList < Base
+      API_ENDPOINT = 'https://api.eveonline.com/char/ContactList.xml.aspx'
+
+      attr_reader :key_id, :v_code, :character_id
+
+      def initialize(key_id, v_code, character_id)
+        super()
+        @key_id = key_id
+        @v_code = v_code
+        @character_id = character_id
+      end
+
+      def url
+        "#{ API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }"
+      end
+    end
+  end
+end
--- lib/eve_online/contact_list.rb
+++
@@ -1,6 +0,0 @@
-# TODO: finish this
-module EveOnline
-  # http://wiki.eve-id.net/APIv2_Char_ContactList_XML
-  class ContactList < Base
-  end
-end
