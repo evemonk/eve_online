e27fe4f89016952e05f2f9d6dd229f705ce40818
Igor Zubkov <igor.zubkov@gmail.com>
2015-12-21 23:16:12 +0200

    Add dummy EveOnline::Characters::ContactNotifications

--- lib/eve_online/characters/contact_notifications.rb
+++ lib/eve_online/characters/contact_notifications.rb
@@ -1,6 +1,21 @@
-# TODO: finish this
 module EveOnline
-  # http://wiki.eve-id.net/APIv2_Char_ContactNotifications_XML
-  class ContactNotifications < Base
+  module Characters
+    # http://wiki.eve-id.net/APIv2_Char_ContactNotifications_XML
+    class ContactNotifications < Base
+      API_ENDPOINT = 'https://api.eveonline.com/char/ContactNotifications.xml.aspx'
+
+      attr_reader :key_id, :v_code, :character_id
+
+      def initialize(key_id, v_code, character_id)
+        super()
+        @key_id = key_id
+        @v_code = v_code
+        @character_id = character_id
+      end
+
+      def url
+        "#{ API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }"
+      end
+    end
   end
 end
