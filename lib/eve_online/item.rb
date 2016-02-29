28069c100912121d8680e46d707610c743049d21
Igor Zubkov <igor.zubkov@gmail.com>
2016-03-01 01:00:34 +0200

    Add EveOnline::Item class for handle items in AssetList

---
+++ lib/eve_online/item.rb
@@ -0,0 +1,58 @@
+module EveOnline
+  class Item
+    attr_reader :options
+
+    def initialize(options = {})
+      @options = options
+    end
+
+    def as_json(*args)
+      {
+        item_id: item_id,
+        location_id: location_id,
+        type_id: type_id,
+        quantity: quantity,
+        flag: flag,
+        singleton: singleton,
+        raw_quantity: raw_quantity
+      }
+    end
+
+    def item_id
+      @item_id ||= options.fetch('@itemID').to_i
+    end
+
+    def location_id
+      @location_id ||= options.fetch('@location_id').to_i
+    end
+
+    def type_id
+      @type_id ||= options.fetch('@typeID').to_i
+    end
+
+    def quantity
+      @quantity ||= options.fetch('@quantity').to_i
+    end
+
+    def flag
+      @flag ||= options.fetch('@flag').to_i
+    end
+
+    def singleton
+      @singleton ||= options.fetch('@singleton').to_i
+    end
+
+    def raw_quantity
+      @raw_quantity ||= options.fetch('@rawQuantity').to_i
+    end
+
+    # => {"@itemID"=>"408887580",
+    #     "@locationID"=>"60000634",
+    #     "@typeID"=>"588",
+    #     "@quantity"=>"1",
+    #     "@flag"=>"4",
+    #     "@singleton"=>"1",
+    #     "@rawQuantity"=>"-1"}
+
+  end
+end
