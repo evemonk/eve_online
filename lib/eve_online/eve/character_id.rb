6e313496204c0bbac9ef06aedc2737ccb2c004ed
Igor Zubkov <igor.zubkov@gmail.com>
2016-03-17 22:00:14 +0200

    CharacterID

--- lib/eve_online/eve/character_id.rb
+++ lib/eve_online/eve/character_id.rb
@@ -4,9 +4,26 @@ module EveOnline
     class CharacterID < Base
       API_ENDPOINT = 'https://api.eveonline.com/eve/CharacterID.xml.aspx'.freeze
 
-      def url
-        API_ENDPOINT
+      attr_reader :names
+
+      def initialize(names)
+        super()
+        @names = names
       end
+
+      # def characters
+      #
+      # end
+      #
+      # def names_list
+      #   raise RuntimeError unless names.kind_of?(Array)
+      #
+      #   names.join(',')
+      # end
+      #
+      # def url
+      #   "#{ API_ENDPOINT }?names=#{ names_list }"
+      # end
     end
   end
 end
--- spec/eve_online/eve/character_id_spec.rb
+++ spec/eve_online/eve/character_id_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe EveOnline::Eve::CharacterID do
+  let(:names) { double }
+
+  subject { described_class.new(names) }
+
+  specify { expect(subject).to be_a EveOnline::Base }
+
+  specify { expect(described_class::API_ENDPOINT).to eq('https://api.eveonline.com/eve/CharacterID.xml.aspx') }
+
+  describe '#initialize' do
+    its(:names) { should eq(names) }
+  end
 end
