24f9efaf3b9b246b03f83a0047ab6d182ab46497
Igor Zubkov <igor.zubkov@gmail.com>
2016-04-19 20:53:32 +0300

    Extract XML API stuff to EveOnline::BaseXML class

--- lib/eve_online/base_xml.rb
+++ lib/eve_online/base_xml.rb
@@ -1,4 +1,32 @@
+require 'nori'
+require 'active_support/time'
+
 module EveOnline
   class BaseXML < Base
+    attr_reader :parser
+
+    def initialize
+      @parser = Nori.new(advanced_typecasting: false)
+    end
+
+    def result
+      @result ||= eveapi.fetch('result')
+    end
+
+    def cached_until
+      @cached_until ||= ActiveSupport::TimeZone['UTC'].parse(eveapi.fetch('cachedUntil'))
+    end
+
+    def current_time
+      @current_time ||= ActiveSupport::TimeZone['UTC'].parse(eveapi.fetch('currentTime'))
+    end
+
+    def version
+      eveapi.fetch('@version').to_i
+    end
+
+    def eveapi
+      @eveapi ||= response.fetch('eveapi')
+    end
   end
 end
