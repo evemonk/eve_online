9374b8f2dd6730e5b9c61a8328d7cdc0ab626740
Igor Zubkov <igor.zubkov@gmail.com>
2016-07-02 00:15:51 +0300

    Add specs

--- spec/eve_online/characters/character_sheet_spec.rb
+++ spec/eve_online/characters/character_sheet_spec.rb
@@ -32,6 +32,373 @@ describe EveOnline::Characters::CharacterSheet do
     its(:character_id) { should eq(character_id) }
   end
 
+  # def as_json
+  #   {
+  #       character_id: character_id,
+  #       name: name,
+  #       home_station_id: home_station_id,
+  #       dob: dob,
+  #       race: race,
+  #       blood_line_id: blood_line_id,
+  #       blood_line: blood_line,
+  #       ancestry_id: ancestry_id,
+  #       ancestry: ancestry,
+  #       gender: gender,
+  #       corporation_name: corporation_name,
+  #       corporation_id: corporation_id,
+  #       alliance_name: alliance_name,
+  #       alliance_id: alliance_id,
+  #       faction_name: faction_name,
+  #       faction_id: faction_id,
+  #       clone_type_id: clone_type_id,
+  #       clone_name: clone_name,
+  #       clone_skill_points: clone_skill_points,
+  #       free_skill_points: free_skill_points,
+  #       free_respecs: free_respecs,
+  #       clone_jump_date: clone_jump_date,
+  #       last_respec_date: last_respec_date,
+  #       last_timed_respec: last_timed_respec,
+  #       remote_station_date: remote_station_date
+  #   }
+  # end
+
+  describe '#name' do
+    before do
+      #
+      # subject.result.fetch('name')
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('name')
+        end
+      end
+    end
+
+    specify { expect { subject.name }.not_to raise_error }
+  end
+
+  # def home_station_id
+  #   @home_station_id ||= result.fetch('homeStationID').to_i
+  # end
+  #
+  # def dob
+  #   @dob ||= ActiveSupport::TimeZone['UTC'].parse(result.fetch('DoB'))
+  # end
+
+  describe '#race' do
+    before do
+      #
+      # subject.result.fetch('race')
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('race')
+        end
+      end
+    end
+
+    specify { expect { subject.race }.not_to raise_error }
+  end
+
+  # def blood_line_id
+  #   @blood_line_id ||= result.fetch('bloodLineID').to_i
+  # end
+
+  describe '#blood_line' do
+    before do
+      #
+      # subject.result.fetch('bloodLine')
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('bloodLine')
+        end
+      end
+    end
+
+    specify { expect { subject.blood_line }.not_to raise_error }
+  end
+
+  # def ancestry_id
+  #   @ancestry_id ||= result.fetch('ancestryID').to_i
+  # end
+
+  describe '#ancestry' do
+    before do
+      #
+      # subject.result.fetch('ancestry')
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('ancestry')
+        end
+      end
+    end
+
+    specify { expect { subject.ancestry }.not_to raise_error }
+  end
+
+  describe '#gender' do
+    before do
+      #
+      # subject.result.fetch('gender')
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('gender')
+        end
+      end
+    end
+
+    specify { expect { subject.gender }.not_to raise_error }
+  end
+
+  describe '#corporation_name' do
+    before do
+      #
+      # subject.result.fetch('corporationName')
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('corporationName')
+        end
+      end
+    end
+
+    specify { expect { subject.corporation_name }.not_to raise_error }
+  end
+
+  # def corporation_id
+  #   @corporation_id ||= result.fetch('corporationID').to_i
+  # end
+
+  describe '#alliance_name' do
+    before do
+      #
+      # subject.result.fetch('allianceName')
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('allianceName')
+        end
+      end
+    end
+
+    specify { expect { subject.alliance_name }.not_to raise_error }
+  end
+
+  # def alliance_id
+  #   @alliance_id ||= result.fetch('allianceID').to_i
+  # end
+
+  describe '#faction_name' do
+    before do
+      #
+      # subject.result.fetch('factionName')
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('factionName')
+        end
+      end
+    end
+
+    specify { expect { subject.faction_name }.not_to raise_error }
+  end
+
+
+  # def faction_id
+  #   @faction_id ||= result.fetch('factionID').to_i
+  # end
+  #
+  # def clone_type_id
+  #   @clone_type_id ||= result.fetch('cloneTypeID').to_i
+  # end
+
+  describe '#clone_name' do
+    before do
+      #
+      # subject.result.fetch('cloneName')
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('cloneName')
+        end
+      end
+    end
+
+    specify { expect { subject.clone_name }.not_to raise_error }
+  end
+
+  describe '#clone_skill_points' do
+    before do
+      #
+      # subject.result.fetch('cloneSkillPoints').to_i
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('cloneSkillPoints') do
+            double.tap do |b|
+              expect(b).to receive(:to_i)
+            end
+          end
+        end
+      end
+    end
+
+    specify { expect { subject.clone_skill_points }.not_to raise_error }
+  end
+
+  describe '#free_skill_points' do
+    before do
+      #
+      # subject.result.fetch('freeSkillPoints').to_i
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('freeSkillPoints') do
+            double.tap do |b|
+              expect(b).to receive(:to_i)
+            end
+          end
+        end
+      end
+    end
+
+    specify { expect { subject.free_skill_points }.not_to raise_error }
+  end
+
+  describe '#free_respecs' do
+    before do
+      #
+      # subject.result.fetch('freeRespecs').to_i
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('freeRespecs') do
+            double.tap do |b|
+              expect(b).to receive(:to_i)
+            end
+          end
+        end
+      end
+    end
+
+    specify { expect { subject.free_respecs }.not_to raise_error }
+  end
+
+  describe '#clone_jump_date' do
+    let(:clone_jump_date) { double }
+
+    before do
+      #
+      # subject.result.fetch('cloneJumpDate') => clone_jump_date
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('cloneJumpDate').and_return(clone_jump_date)
+        end
+      end
+    end
+
+    before do
+      #
+      # ActiveSupport::TimeZone['UTC'].parse(clone_jump_date)
+      #
+      expect(ActiveSupport::TimeZone).to receive(:[]).with('UTC') do
+        double.tap do |a|
+          expect(a).to receive(:parse).with(clone_jump_date)
+        end
+      end
+    end
+
+    specify { expect { subject.clone_jump_date }.not_to raise_error }
+  end
+
+  describe '#last_respec_date' do
+    let(:last_respec_date) { double }
+
+    before do
+      #
+      # subject.result.fetch('lastRespecDate') => last_respec_date
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('lastRespecDate').and_return(last_respec_date)
+        end
+      end
+    end
+
+    before do
+      #
+      # ActiveSupport::TimeZone['UTC'].parse(last_respec_date)
+      #
+      expect(ActiveSupport::TimeZone).to receive(:[]).with('UTC') do
+        double.tap do |a|
+          expect(a).to receive(:parse).with(last_respec_date)
+        end
+      end
+    end
+
+    specify { expect { subject.last_respec_date }.not_to raise_error }
+  end
+
+  describe '#last_timed_respec' do
+    let(:last_timed_respec) { double }
+
+    before do
+      #
+      # subject.result.fetch('lastTimedRespec') => last_timed_respec
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('lastTimedRespec').and_return(last_timed_respec)
+        end
+      end
+    end
+
+    before do
+      #
+      # ActiveSupport::TimeZone['UTC'].parse(last_timed_respec)
+      #
+      expect(ActiveSupport::TimeZone).to receive(:[]).with('UTC') do
+        double.tap do |a|
+          expect(a).to receive(:parse).with(last_timed_respec)
+        end
+      end
+    end
+
+    specify { expect { subject.last_timed_respec }.not_to raise_error }
+  end
+
+  describe '#remote_station_date' do
+    let(:remote_station_date) { double }
+
+    before do
+      #
+      # subject.result.fetch('remoteStationDate') => remote_station_date
+      #
+      expect(subject).to receive(:result) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('remoteStationDate').and_return(remote_station_date)
+        end
+      end
+    end
+
+    before do
+      #
+      # ActiveSupport::TimeZone['UTC'].parse(remote_station_date)
+      #
+      expect(ActiveSupport::TimeZone).to receive(:[]).with('UTC') do
+        double.tap do |a|
+          expect(a).to receive(:parse).with(remote_station_date)
+        end
+      end
+    end
+
+    specify { expect { subject.remote_station_date }.not_to raise_error }
+  end
+
   describe '#url' do
     specify do
       expect(subject.url).to eq("#{ described_class::API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }")
