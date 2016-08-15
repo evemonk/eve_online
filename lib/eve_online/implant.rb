df4f1a9373a889df47f1091eb4561efdd6d811e9
Igor Zubkov <igor.zubkov@gmail.com>
2016-08-15 12:39:47 +0300

    Implement EveOnline::Implant

--- lib/eve_online/implant.rb
+++ lib/eve_online/implant.rb
@@ -5,5 +5,20 @@ module EveOnline
     def initialize(options)
       @options = options
     end
+
+    def as_json
+      {
+        type_id: type_id,
+        type_name: type_name
+      }
+    end
+
+    def type_id
+      @type_id ||= options.fetch('@typeID').to_i
+    end
+
+    def type_name
+      @type_name ||= options.fetch('@typeName')
+    end
   end
 end
