4e0a6a39ff74bd90f4a8958ee8ae284c372ecef8
Igor Zubkov <igor.zubkov@gmail.com>
2016-08-18 18:12:36 +0300

    Implement EveOnline::Standing

--- lib/eve_online/standing.rb
+++ lib/eve_online/standing.rb
@@ -5,5 +5,25 @@ module EveOnline
     def initialize(options)
       @options = options
     end
+
+    def as_json
+      {
+        from_id: from_id,
+        from_name: from_name,
+        standing: standing
+      }
+    end
+
+    def from_id
+      @from_id ||= options.fetch('@fromID').to_i
+    end
+
+    def from_name
+      @from_name ||= options.fetch('@fromName')
+    end
+
+    def standing
+      @standing ||= options.fetch('@standing').to_f
+    end
   end
 end
--- spec/eve_online/standing_spec.rb
+++ spec/eve_online/standing_spec.rb
@@ -8,4 +8,89 @@ describe EveOnline::Standing do
 
     its(:options) { should eq(options) }
   end
+
+  describe '#as_json' do
+    let(:options) { double }
+
+    let(:standing) { described_class.new(options) }
+
+    before { expect(standing).to receive(:from_id).and_return(3_008_771) }
+
+    before { expect(standing).to receive(:from_name).and_return('Nehrnah Gorouyar') }
+
+    before { expect(standing).to receive(:standing).and_return(0.12) }
+
+    subject { standing.as_json }
+
+    its([:from_id]) { should eq(3_008_771) }
+
+    its([:from_name]) { should eq('Nehrnah Gorouyar') }
+
+    its([:standing]) { should eq(0.12) }
+  end
+
+  describe '#from_id' do
+    let(:options) { double }
+
+    subject { described_class.new(options) }
+
+    before do
+      #
+      # subject.options.fetch('@fromID').to_i
+      #
+      expect(subject).to receive(:options) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('@fromID') do
+            double.tap do |b|
+              expect(b).to receive(:to_i)
+            end
+          end
+        end
+      end
+    end
+
+    specify { expect { subject.from_id }.not_to raise_error }
+  end
+
+  describe '#from_name' do
+    let(:options) { double }
+
+    subject { described_class.new(options) }
+
+    before do
+      #
+      # subject.options.fetch('@fromName')
+      #
+      expect(subject).to receive(:options) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('@fromName')
+        end
+      end
+    end
+
+    specify { expect { subject.from_name }.not_to raise_error }
+  end
+
+  describe '#standing' do
+    let(:options) { double }
+
+    subject { described_class.new(options) }
+
+    before do
+      #
+      # subject.options.fetch('@standing').to_f
+      #
+      expect(subject).to receive(:options) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('@standing') do
+            double.tap do |b|
+              expect(b).to receive(:to_f)
+            end
+          end
+        end
+      end
+    end
+
+    specify { expect { subject.standing }.not_to raise_error }
+  end
 end
