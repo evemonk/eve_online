89e8ab3eb301bf775a41ca9980a8b03f587b6341
Igor Zubkov <igor.zubkov@gmail.com>
2016-08-19 12:12:21 +0300

    Add imlementation

--- lib/eve_online/characters/standings.rb
+++ lib/eve_online/characters/standings.rb
@@ -13,8 +13,52 @@ module EveOnline
         @character_id = character_id
       end
 
+      # def agents
+      #   @agents ||= begin
+      #     output = []
+      #     agents_rowset.each do |agent|
+      #       output << Standing.new(agent)
+      #     end
+      #     output
+      #   end
+      # end
+      #
+      # def npc_corporations
+      #   @npc_corporations ||= begin
+      #     output = []
+      #     npc_corporations_rowset.each do |agent|
+      #       output << Standing.new(agent)
+      #     end
+      #     output
+      #   end
+      # end
+      #
+      # def factions
+      #   @factions ||= begin
+      #     output = []
+      #     factions_rowset.each do |agent|
+      #       output << Standing.new(agent)
+      #     end
+      #     output
+      #   end
+      # end
+
       def url
         "#{ API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }"
+      end
+
+      private
+
+      def agents_rowset
+        @agents_rowset ||= result.fetch('characterNPCStandings').fetch('rowset').reject { |a| a.fetch('@name') != 'agents' }.first.fetch('row')
+      end
+
+      def npc_corporations_rowset
+        @npc_corporations_rowset ||= result.fetch('characterNPCStandings').fetch('rowset').reject { |a| a.fetch('@name') != 'NPCCorporations' }.first.fetch('row')
+      end
+
+      def factions_rowset
+        @factions_rowset ||= result.fetch('characterNPCStandings').fetch('rowset').reject { |a| a.fetch('@name') != 'factions' }.first.fetch('row')
       end
     end
   end
