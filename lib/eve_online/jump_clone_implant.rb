c3547cf55d38e1deae979f5cfb74051dab6f98e6
Igor Zubkov <igor.zubkov@gmail.com>
2016-08-23 12:38:17 +0300

    Finish EveOnline::JumpCloneImplant

--- lib/eve_online/jump_clone_implant.rb
+++ lib/eve_online/jump_clone_implant.rb
@@ -5,5 +5,25 @@ module EveOnline
     def initialize(options)
       @options = options
     end
+
+    def as_json
+      {
+        jump_clone_id: jump_clone_id,
+        type_id: type_id,
+        type_name: type_name
+      }
+    end
+
+    def jump_clone_id
+      @jump_clone_id ||= options.fetch('@jumpCloneID').to_i
+    end
+
+    def type_id
+      @type_id ||= options.fetch('@typeID').to_i
+    end
+
+    def type_name
+      @type_name ||= options.fetch('@typeName')
+    end
   end
 end
