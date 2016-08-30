4b9134bd6c0e9ce6c04d8b09f609fe04fff4eb39
Igor Zubkov <igor.zubkov@gmail.com>
2016-08-30 14:06:08 +0300

    EveOnline::SDE::TypeIDs#type_ids

--- lib/eve_online/sde/type_ids.rb
+++ lib/eve_online/sde/type_ids.rb
@@ -7,6 +7,16 @@ module EveOnline
         @file = file
       end
 
+      def type_ids
+        @type_ids ||= begin
+          output = []
+          content.each do |entry|
+            output << TypeID.new(entry)
+          end
+          output
+        end
+      end
+
       def content
         @content ||= YAML.load(File.open(file))
       end
