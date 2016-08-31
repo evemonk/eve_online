b2615e9851a9e30cd794d6e248483f2087ea9b81
Igor Zubkov <igor.zubkov@gmail.com>
2016-08-31 16:55:05 +0300

    Cleanup

--- spec/eve_online/characters/contact_notifications_spec.rb
+++ spec/eve_online/characters/contact_notifications_spec.rb
@@ -115,21 +115,6 @@ describe EveOnline::Characters::ContactNotifications do
 
   # private methods
 
-  describe '#row' do
-    before do
-      #
-      # subject.rowset.fetch('row')
-      #
-      expect(subject).to receive(:rowset) do
-        double.tap do |a|
-          expect(a).to receive(:fetch).with('row')
-        end
-      end
-    end
-
-    specify { expect { subject.send(:row) }.not_to raise_error }
-  end
-
   describe '#rowset' do
     before do
       #
@@ -143,5 +128,20 @@ describe EveOnline::Characters::ContactNotifications do
     end
 
     specify { expect { subject.send(:rowset) }.not_to raise_error }
+  end
+
+  describe '#row' do
+    before do
+      #
+      # subject.rowset.fetch('row')
+      #
+      expect(subject).to receive(:rowset) do
+        double.tap do |a|
+          expect(a).to receive(:fetch).with('row')
+        end
+      end
+    end
+
+    specify { expect { subject.send(:row) }.not_to raise_error }
   end
 end
