841e921f9f86a126ca37a4108ebaf26fafce781f
Igor Zubkov <igor.zubkov@gmail.com>
2016-09-02 14:54:11 +0300

    Add accountKey, fromID and rowCount to EveOnline::Characters::WalletJournal#initialize

--- lib/eve_online/characters/wallet_journal.rb
+++ lib/eve_online/characters/wallet_journal.rb
@@ -6,21 +6,47 @@ module EveOnline
 
       ACCESS_MASK = 2097152
 
-      attr_reader :key_id, :v_code, :character_id #, :from_id, :row_count
+      attr_reader :key_id, :v_code, :character_id, :account_key, :from_id, :row_count
 
-      def initialize(key_id, v_code, character_id) #, options = {})
+      def initialize(key_id, v_code, character_id, options = {})
         super()
         @key_id = key_id
         @v_code = v_code
         @character_id = character_id
-        # @from_id = options.fetch(:from_id, nil)
-        # @row_count = options.fetch(:row_count, nil)
+        @account_key = options.fetch(:account_key, 1000)
+        @from_id = options.fetch(:from_id, nil)
+        @row_count = options.fetch(:row_count, nil)
       end
+
+      # def wallet_journal_entries
+      #   case row
+      #   when Hash
+      #     [WalletJournalEntry.new(row)]
+      #   when Array
+      #     output = []
+      #     row.each do |blueprint|
+      #       output << WalletJournalEntry.new(blueprint)
+      #     end
+      #     output
+      #   else
+      #     raise ArgumentError
+      #   end
+      # end
 
       def url
         "#{ API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }"
         # "#{ API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }&rowCount=#{ row_count }"
       end
+
+      # private
+      #
+      # def rowset
+      #   @rowset ||= result.fetch('rowset')
+      # end
+      #
+      # def row
+      #   @row ||= rowset.fetch('row')
+      # end
     end
   end
 end
--- spec/eve_online/characters/wallet_journal_spec.rb
+++ spec/eve_online/characters/wallet_journal_spec.rb
@@ -16,22 +16,48 @@ describe EveOnline::Characters::WalletJournal do
   specify { expect(described_class::ACCESS_MASK).to eq(2097152) }
 
   describe '#initialize' do
-    let(:parser) { double }
+    context 'default' do
+      let(:parser) { double }
 
-    before do
-      #
-      # Nori.new(advanced_typecasting: false) => double
-      #
-      expect(Nori).to receive(:new).with(advanced_typecasting: false).and_return(parser)
+      before do
+        #
+        # Nori.new(advanced_typecasting: false) => double
+        #
+        expect(Nori).to receive(:new).with(advanced_typecasting: false).and_return(parser)
+      end
+
+      its(:parser) { should eq(parser) }
+
+      its(:key_id) { should eq(key_id) }
+
+      its(:v_code) { should eq(v_code) }
+
+      its(:character_id) { should eq(character_id) }
+
+      its(:account_key) { should eq(1000) }
+
+      its(:from_id) { should eq(nil) }
+
+      its(:row_count) { should eq(nil) }
     end
 
-    its(:parser) { should eq(parser) }
+    context 'with options' do
+      let(:options) do
+        {
+          account_key: 1234,
+          from_id: 12345,
+          row_count: 512
+        }
+      end
 
-    its(:key_id) { should eq(key_id) }
+      subject { described_class.new(key_id, v_code, character_id, options) }
 
-    its(:v_code) { should eq(v_code) }
+      its(:account_key) { should eq(1234) }
 
-    its(:character_id) { should eq(character_id) }
+      its(:from_id) { should eq(12345) }
+
+      its(:row_count) { should eq(512) }
+    end
   end
 
   describe '#url' do
