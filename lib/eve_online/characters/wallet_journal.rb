7df41dabd7aae067bced25dee8d3c2d1e8cbf799
Igor Zubkov <igor.zubkov@gmail.com>
2016-09-02 13:01:36 +0300

    Comment new stuff

--- lib/eve_online/characters/wallet_journal.rb
+++ lib/eve_online/characters/wallet_journal.rb
@@ -6,20 +6,20 @@ module EveOnline
 
       ACCESS_MASK = 2097152
 
-      attr_reader :key_id, :v_code, :character_id, :from_id, :row_count
+      attr_reader :key_id, :v_code, :character_id #, :from_id, :row_count
 
-      def initialize(key_id, v_code, character_id, options = {})
+      def initialize(key_id, v_code, character_id) #, options = {})
         super()
         @key_id = key_id
         @v_code = v_code
         @character_id = character_id
-        @from_id = options.fetch(:from_id, nil)
-        @row_count = options.fetch(:row_count, nil)
+        # @from_id = options.fetch(:from_id, nil)
+        # @row_count = options.fetch(:row_count, nil)
       end
 
       def url
-        # "#{ API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }"
-        "#{ API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }&rowCount=#{ row_count }"
+        "#{ API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }"
+        # "#{ API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }&rowCount=#{ row_count }"
       end
     end
   end
