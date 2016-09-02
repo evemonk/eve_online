af760e52526c5893a225adb3302e874298b603c5
Igor Zubkov <igor.zubkov@gmail.com>
2016-09-02 14:43:51 +0300

    More specs

--- spec/eve_online/wallet_journal_entry_spec.rb
+++ spec/eve_online/wallet_journal_entry_spec.rb
@@ -9,26 +9,79 @@ describe EveOnline::WalletJournalEntry do
     its(:options) { should eq(options) }
   end
 
-  # def as_json
-  #   {
-  #       date: date,
-  #       ref_id: ref_id,
-  #       ref_type_id: ref_type_id,
-  #       owner_name1: owner_name1,
-  #       owner_id1: owner_id1,
-  #       owner_name2: owner_name2,
-  #       owner_id2: owner_id2,
-  #       arg_name1: arg_name1,
-  #       arg_id1: arg_id1,
-  #       amount: amount,
-  #       balance: balance,
-  #       reason: reason,
-  #       tax_receiver_id: tax_receiver_id,
-  #       tax_amount: tax_amount,
-  #       owner1_type_id: owner1_type_id,
-  #       owner2_type_id: owner2_type_id
-  #   }
-  # end
+  describe '#as_json' do
+    let(:options) { double }
+
+    let(:wallet_journal_entry) { described_class.new(options) }
+
+    let(:date) { double }
+
+    before { expect(wallet_journal_entry).to receive(:date).and_return(date) }
+
+    before { expect(wallet_journal_entry).to receive(:ref_id).and_return(6709813912) }
+
+    before { expect(wallet_journal_entry).to receive(:ref_type_id).and_return(15) }
+
+    before { expect(wallet_journal_entry).to receive(:owner_name1).and_return('reygar burnt') }
+
+    before { expect(wallet_journal_entry).to receive(:owner_id1).and_return(1801683792) }
+
+    before { expect(wallet_journal_entry).to receive(:owner_name2).and_return('Wiyrkomi Corporation') }
+
+    before { expect(wallet_journal_entry).to receive(:owner_id2).and_return(1000011) }
+
+    before { expect(wallet_journal_entry).to receive(:arg_name1).and_return('EVE System') }
+
+    before { expect(wallet_journal_entry).to receive(:arg_id1).and_return(1) }
+
+    before { expect(wallet_journal_entry).to receive(:amount).and_return(-9250.00) }
+
+    before { expect(wallet_journal_entry).to receive(:balance).and_return(385574791.30) }
+
+    before { expect(wallet_journal_entry).to receive(:reason).and_return('some-reason') }
+
+    before { expect(wallet_journal_entry).to receive(:tax_receiver_id).and_return('123') }
+
+    before { expect(wallet_journal_entry).to receive(:tax_amount).and_return('tax-amount') }
+
+    before { expect(wallet_journal_entry).to receive(:owner1_type_id).and_return(1380) }
+
+    before { expect(wallet_journal_entry).to receive(:owner2_type_id).and_return(2) }
+
+    subject { wallet_journal_entry.as_json }
+
+    its([:date]) { should eq(date) }
+
+    its([:ref_id]) { should eq(6709813912) }
+
+    its([:ref_type_id]) { should eq(15) }
+
+    its([:owner_name1]) { should eq('reygar burnt') }
+
+    its([:owner_id1]) { should eq(1801683792) }
+
+    its([:owner_name2]) { should eq('Wiyrkomi Corporation') }
+
+    its([:owner_id2]) { should eq(1000011) }
+
+    its([:arg_name1]) { should eq('EVE System') }
+
+    its([:arg_id1]) { should eq(1) }
+
+    its([:amount]) { should eq(-9250.00) }
+
+    its([:balance]) { should eq(385574791.30) }
+
+    its([:reason]) { should eq('some-reason') }
+
+    its([:tax_receiver_id]) { should eq('123') }
+
+    its([:tax_amount]) { should eq('tax-amount') }
+
+    its([:owner1_type_id]) { should eq(1380) }
+
+    its([:owner2_type_id]) { should eq(2) }
+  end
 
   describe '#date' do
     let(:options) { double }
