834dd480598a497fa05c7ada402170f504c9630a
Igor Zubkov <igor.zubkov@gmail.com>
2016-09-05 15:14:14 +0300

    Cleanup

--- spec/eve_online/characters/medals_spec.rb
+++ spec/eve_online/characters/medals_spec.rb
@@ -13,7 +13,7 @@ describe EveOnline::Characters::Medals do
 
   specify { expect(described_class::API_ENDPOINT).to eq('https://api.eveonline.com/char/Medals.xml.aspx') }
 
-  specify { expect(described_class::ACCESS_MASK).to eq(8192) }
+  specify { expect(described_class::ACCESS_MASK).to eq(8_192) }
 
   describe '#initialize' do
     let(:parser) { double }
