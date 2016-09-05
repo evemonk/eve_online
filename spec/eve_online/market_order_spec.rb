d6159b93fd078161a92ef6be429c19cb607ace89
Igor Zubkov <igor.zubkov@gmail.com>
2016-09-05 13:06:19 +0300

    One more spec

--- spec/eve_online/market_order_spec.rb
+++ spec/eve_online/market_order_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe EveOnline::MarketOrder do
+  describe '#initialize' do
+    let(:options) { double }
+
+    subject { described_class.new(options) }
+
+    its(:options) { should eq(options) }
+  end
 end
