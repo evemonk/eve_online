01a0d9054d6aabc0d438af54cb0f49d437bc323f
Igor Zubkov <igor.zubkov@gmail.com>
2016-09-05 18:12:22 +0300

    Cleanup

--- lib/eve_online/base.rb
+++ lib/eve_online/base.rb
@@ -12,20 +12,17 @@ module EveOnline
     def user_agent
       "EveOnline API (https://github.com/biow0lf/eve_online) v#{ VERSION }"
     end
-
     memoize :user_agent
 
     def content
       open(url, open_timeout: 60, read_timeout: 60,
                 'User-Agent' => user_agent).read
     end
-
     memoize :content
 
     def response
       parser.parse(content)
     end
-
     memoize :response
   end
 end
