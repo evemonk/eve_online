d602a45450ec6736c5ea5818fa64acfa15b25ea3
Igor Zubkov <igor.zubkov@gmail.com>
2016-09-07 17:37:59 +0300

    Memoizate EveOnline::CharacterJumpClones

--- lib/eve_online/character_jump_clones.rb
+++ lib/eve_online/character_jump_clones.rb
@@ -1,5 +1,9 @@
+require 'memoist'
+
 module EveOnline
   class CharacterJumpClones
+    extend Memoist
+
     attr_reader :result
 
     def initialize(result)
@@ -13,11 +17,13 @@ module EveOnline
       end
       output
     end
+    memoize :jump_clones
 
     private
 
     def rowset
       result.fetch('rowset').reject { |a| a.fetch('@name') != 'jumpClones' }.first.fetch('row')
     end
+    memoize :rowset
   end
 end
