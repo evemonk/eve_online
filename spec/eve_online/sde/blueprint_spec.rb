1168ea07e319f96f1fecd6552b98d4c404bb11a8
Igor Zubkov <igor.zubkov@gmail.com>
2016-09-16 00:35:28 +0300

    Fix spec

--- spec/eve_online/sde/blueprint_spec.rb
+++ spec/eve_online/sde/blueprint_spec.rb
@@ -12,17 +12,23 @@ describe EveOnline::SDE::Blueprint do
   describe '#as_json' do
     let(:options) { double }
 
+    let(:activities) { double }
+
     let(:blueprint) { described_class.new(options) }
 
     before { expect(blueprint).to receive(:type_id).and_return(681) }
 
     before { expect(blueprint).to receive(:max_production_limit).and_return(300) }
 
+    before { expect(blueprint).to receive(:activities).and_return(activities) }
+
     subject { blueprint.as_json }
 
     its([:type_id]) { should eq(681) }
 
     its([:max_production_limit]) { should eq(300) }
+
+    its([:activities]) { should eq(activities) }
   end
 
   describe '#type_id' do
