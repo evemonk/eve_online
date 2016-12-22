749870ee954956d454f0d5b439384501c1fa786b
Igor Zubkov <igor.zubkov@gmail.com>
2016-12-22 18:02:52 +0200

    Add items

--- lib/eve_online/sde/items.rb
+++ lib/eve_online/sde/items.rb
@@ -1,6 +1,14 @@
 module EveOnline
   module SDE
     class Items < Base
+      def items
+        output = []
+        data.each do |item|
+          output << EveOnline::SDE::Models::Item.new(item)
+        end
+        output
+      end
+      memoize :items
     end
   end
 end
--- lib/eve_online/sde/models/item.rb
+++ lib/eve_online/sde/models/item.rb
@@ -2,6 +2,46 @@ module EveOnline
   module SDE
     module Models
       class Item
+        attr_reader :data
+
+        def initialize(data)
+          @data = data
+        end
+
+        def as_json
+          {
+            flag_id: flag_id,
+            item_id: item_id,
+            location_id: location_id,
+            owner_id: owner_id,
+            quantity: quantity,
+            type_id: type_id
+          }
+        end
+
+        def flag_id
+          data['flagID']
+        end
+
+        def item_id
+          data['itemID']
+        end
+
+        def location_id
+          data['locationID']
+        end
+
+        def owner_id
+          data['ownerID']
+        end
+
+        def quantity
+          data['quantity']
+        end
+
+        def type_id
+          data['typeID']
+        end
       end
     end
   end
