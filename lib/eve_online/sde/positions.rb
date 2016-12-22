976196804ea6ed3eb3db9d3552f3d9e2b104543b
Igor Zubkov <igor.zubkov@gmail.com>
2016-12-22 21:07:02 +0200

    SDE positions

--- lib/eve_online/sde/positions.rb
+++ lib/eve_online/sde/positions.rb
@@ -1,6 +1,14 @@
 module EveOnline
   module SDE
     class Positions < Base
+      def positions
+        output = []
+        data.each do |position|
+          output << EveOnline::SDE::Models::Position.new(position)
+        end
+        output
+      end
+      memoize :positions
     end
   end
 end
