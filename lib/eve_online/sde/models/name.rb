bd879d584d4b2dddc36d357b709aa08fe012118e
Igor Zubkov <igor.zubkov@gmail.com>
2016-12-22 18:25:56 +0200

    SDE names

--- lib/eve_online/sde/models/name.rb
+++ lib/eve_online/sde/models/name.rb
@@ -2,6 +2,26 @@ module EveOnline
   module SDE
     module Models
       class Name
+        attr_reader :data
+
+        def initialize(data)
+          @data = data
+        end
+
+        def as_json
+          {
+            item_id: item_id,
+            item_name: item_name
+          }
+        end
+
+        def item_id
+          data['itemID']
+        end
+
+        def item_name
+          data['itemName']
+        end
       end
     end
   end
--- lib/eve_online/sde/names.rb
+++ lib/eve_online/sde/names.rb
@@ -1,6 +1,14 @@
 module EveOnline
   module SDE
     class Names < Base
+      def names
+        output = []
+        data.each do |name|
+          output << EveOnline::SDE::Models::Name.new(name)
+        end
+        output
+      end
+      memoize :names
     end
   end
 end
