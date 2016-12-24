0119beedc4f0c1ef78854139ad730cc7d5600c70
Igor Zubkov <igor.zubkov@gmail.com>
2016-12-24 13:03:05 +0200

    Add spec

--- spec/eve_online/sde/inv_positions_spec.rb
+++ spec/eve_online/sde/inv_positions_spec.rb
@@ -1,4 +1,28 @@
 require 'spec_helper'
 
 describe EveOnline::SDE::InvPositions do
+  let(:file) { double }
+
+  subject { described_class.new(file) }
+
+  specify { expect(subject).to be_a(EveOnline::SDE::Base) }
+
+  describe '#inv_positions' do
+    let(:item) { double }
+
+    let(:inv_position) { double }
+
+    before { expect(subject).to receive(:data).and_return([item]) }
+
+    before do
+      #
+      # EveOnline::SDE::Models::InvPosition.new(item) => inv_position
+      #
+      expect(EveOnline::SDE::Models::InvPosition).to receive(:new).with(item).and_return(inv_position)
+    end
+
+    specify { expect(subject.inv_positions).to eq([inv_position]) }
+
+    specify { expect { subject.inv_positions }.to change { subject.instance_variable_defined?(:@_memoized_inv_positions) }.from(false).to(true) }
+  end
 end
