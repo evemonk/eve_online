cfe636d267067a5e9fe85630e87051a77b143316
Igor Zubkov <igor.zubkov@gmail.com>
2016-12-24 13:04:53 +0200

    Add spec

--- spec/eve_online/sde/inv_names_spec.rb
+++ spec/eve_online/sde/inv_names_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe EveOnline::SDE::InvNames do
-  # TODO: write specs
+  let(:file) { double }
+
+  subject { described_class.new(file) }
+
+  specify { expect(subject).to be_a(EveOnline::SDE::Base) }
+
+  describe '#inv_names' do
+    let(:item) { double }
+
+    let(:inv_name) { double }
+
+    before { expect(subject).to receive(:data).and_return([item]) }
+
+    before do
+      #
+      # EveOnline::SDE::Models::InvName.new(item) => inv_name
+      #
+      expect(EveOnline::SDE::Models::InvName).to receive(:new).with(item).and_return(inv_name)
+    end
+
+    specify { expect(subject.inv_names).to eq([inv_name]) }
+
+    specify { expect { subject.inv_names }.to change { subject.instance_variable_defined?(:@_memoized_inv_names) }.from(false).to(true) }
+  end
 end
