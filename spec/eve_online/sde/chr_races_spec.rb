6e0b70068166c140fbb3b96082f93e241c2768da
Igor Zubkov <igor.zubkov@gmail.com>
2016-12-24 13:01:36 +0200

    Add spec

--- spec/eve_online/sde/chr_races_spec.rb
+++ spec/eve_online/sde/chr_races_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe EveOnline::SDE::ChrRaces do
-  # TODO: write
+  let(:file) { double }
+
+  subject { described_class.new(file) }
+
+  specify { expect(subject).to be_a(EveOnline::SDE::Base) }
+
+  describe '#chr_races' do
+    let(:item) { double }
+
+    let(:chr_race) { double }
+
+    before { expect(subject).to receive(:data).and_return([item]) }
+
+    before do
+      #
+      # EveOnline::SDE::Models::ChrRace.new(item) => chr_race
+      #
+      expect(EveOnline::SDE::Models::ChrRace).to receive(:new).with(item).and_return(chr_race)
+    end
+
+    specify { expect(subject.chr_races).to eq([chr_race]) }
+
+    specify { expect { subject.chr_races }.to change { subject.instance_variable_defined?(:@_memoized_chr_races) }.from(false).to(true) }
+  end
 end
