8d4f5835b95a9c32665dc1368f4d47bf407ad922
Igor Zubkov <igor.zubkov@gmail.com>
2017-02-15 14:26:41 +0200

    Add token to EveOnline::ESI::Base

--- spec/eve_online/esi/base_spec.rb
+++ spec/eve_online/esi/base_spec.rb
@@ -4,11 +4,25 @@ describe EveOnline::ESI::Base do
   specify { expect(described_class).to be_a(Memoist) }
 
   describe '#initialize' do
-    its(:parser) { should eq(JSON) }
+    context 'without token'do
+      its(:token) { should eq(nil) }
+
+      its(:parser) { should eq(JSON) }
+    end
+
+    context 'with token' do
+      subject { described_class.new('token123') }
+
+      its(:token) { should eq('token123') }
+    end
   end
 
   describe '#url' do
     specify { expect { subject.url }.to raise_error(NotImplementedError) }
+  end
+
+  describe '#scope' do
+    specify { expect { subject.scope }.to raise_error(NotImplementedError) }
   end
 
   describe '#user_agent' do
@@ -17,64 +31,70 @@ describe EveOnline::ESI::Base do
 
   describe '#content' do
     context 'ok' do
-      let(:url) { double }
+      context 'without token' do
+        let(:url) { double }
 
-      before { expect(subject).to receive(:url).and_return(url) }
+        before { expect(subject).to receive(:url).and_return(url) }
 
-      let(:user_agent) { double }
+        let(:user_agent) { double }
 
-      before { expect(subject).to receive(:user_agent).and_return(user_agent) }
+        before { expect(subject).to receive(:user_agent).and_return(user_agent) }
 
-      let(:faraday) { double }
+        let(:faraday) { double }
 
-      before do
-        #
-        # Faraday.new => faraday
-        #
-        expect(Faraday).to receive(:new).and_return(faraday)
-      end
+        before do
+          #
+          # Faraday.new => faraday
+          #
+          expect(Faraday).to receive(:new).and_return(faraday)
+        end
 
-      before do
-        #
-        # faraday.headers[:user_agent] = user_agent
-        #
-        expect(faraday).to receive(:headers) do
-          double.tap do |a|
-            expect(a).to receive(:[]=).with(:user_agent, user_agent)
+        before do
+          #
+          # faraday.headers[:user_agent] = user_agent
+          #
+          expect(faraday).to receive(:headers) do
+            double.tap do |a|
+              expect(a).to receive(:[]=).with(:user_agent, user_agent)
+            end
           end
         end
-      end
 
-      before do
-        expect(faraday).to receive(:options) do
-          double.tap do |a|
-            expect(a).to receive(:timeout=).with(60)
+        before do
+          expect(faraday).to receive(:options) do
+            double.tap do |a|
+              expect(a).to receive(:timeout=).with(60)
+            end
           end
         end
-      end
 
-      before do
-        expect(faraday).to receive(:options) do
-          double.tap do |a|
-            expect(a).to receive(:open_timeout=).with(60)
+        before do
+          expect(faraday).to receive(:options) do
+            double.tap do |a|
+              expect(a).to receive(:open_timeout=).with(60)
+            end
           end
         end
-      end
 
-      before do
-        #
-        # faraday.get(url).body
-        #
-        expect(faraday).to receive(:get).with(url) do
-          double.tap do |a|
-            expect(a).to receive(:body)
+        before do
+          #
+          # faraday.get(url).body
+          #
+          expect(faraday).to receive(:get).with(url) do
+            double.tap do |a|
+              expect(a).to receive(:body)
+            end
           end
         end
+
+        specify { expect { subject.content }.not_to raise_error }
+
+        specify { expect { subject.content }.to change { subject.instance_variable_defined?(:@_memoized_content) }.from(false).to(true) }
       end
 
-      specify { expect { subject.content }.not_to raise_error }
-
-      specify { expect { subject.content }.to change { subject.instance_variable_defined?(:@_memoized_content) }.from(false).to(true) }
+      context 'with token' do
+        # TODO: write
+      end
     end
 
     context 'exception' do
