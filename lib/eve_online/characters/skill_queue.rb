07ca525c8201a1088c696eac40996ba88b40b06b
Igor Zubkov <igor.zubkov@gmail.com>
2017-02-23 15:29:16 +0200

    Use EveOnline::XML::Models::SkillQueueEntry

--- lib/eve_online/characters/skill_queue.rb
+++ lib/eve_online/characters/skill_queue.rb
@@ -18,11 +18,11 @@ module EveOnline
       def skills
         case row
         when Hash
-          [SkillQueueEntry.new(row)]
+          [EveOnline::XML::Models::SkillQueueEntry.new(row)]
         when Array
           output = []
           row.each do |skill|
-            output << SkillQueueEntry.new(skill)
+            output << EveOnline::XML::Models::SkillQueueEntry.new(skill)
           end
           output
         else
