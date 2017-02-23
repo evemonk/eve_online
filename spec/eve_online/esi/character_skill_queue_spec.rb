f1e927f4a868e36e4b31dec3e56a3a4262efed5f
Igor Zubkov <igor.zubkov@gmail.com>
2017-02-23 21:05:27 +0200

    EveOnline::ESI::CharacterSkillQueue#skills

--- spec/eve_online/esi/character_skill_queue_spec.rb
+++ spec/eve_online/esi/character_skill_queue_spec.rb
@@ -19,6 +19,43 @@ describe EveOnline::ESI::CharacterSkillQueue do
     its(:character_id) { should eq(character_id) }
   end
 
+  describe '#skills' do
+    let(:skill_queue_entry) { double }
+
+    let(:response) do
+      [
+        {
+          'skill_id' => 12487,
+          'finished_level' => 3,
+          'queue_position' => 0,
+          'finish_date' => '2017-01-16T03:00:35Z',
+          'start_date' => '2017-01-15T11:38:25Z',
+          'training_start_sp' => 7263,
+          'level_end_sp' => 40000,
+          'level_start_sp' => 7072
+        }
+      ]
+    end
+
+    before do
+      #
+      # subject.response # => [{"skill_id"=>12487, "finished_level"=>3, "queue_position"=>0, "finish_date"=>"2017-01-16T03:00:35Z", "start_date"=>"2017-01-15T11:38:25Z", "training_start_sp"=>7263, "level_end_sp"=>40000, "level_start_sp"=>7072}]
+      #
+      expect(subject).to receive(:response).and_return(response)
+    end
+
+    before do
+      #
+      # EveOnline::ESI::Models::SkillQueueEntry.new(response.first) # => skill_queue_entry
+      #
+      expect(EveOnline::ESI::Models::SkillQueueEntry).to receive(:new).with(response.first).and_return(skill_queue_entry)
+    end
+
+    specify { expect(subject.skills).to eq([skill_queue_entry]) }
+
+    specify { expect { subject.skills }.to change { subject.instance_variable_defined?(:@_memoized_skills) }.from(false).to(true) }
+  end
+
   describe '#scope' do
     specify { expect(subject.scope).to eq('esi-skills.read_skillqueue.v1') }
   end
