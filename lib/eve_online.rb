febbab1ab105b6be6cf488c7c4208572453307bf
Igor Zubkov <igor.zubkov@gmail.com>
2017-05-02 20:29:39 +0300

    Add EveOnline::Exceptions::InvalidCharacterIDException

--- lib/eve_online.rb
+++ lib/eve_online.rb
@@ -3,6 +3,7 @@ require 'eve_online/version'
 require 'eve_online/exceptions/base'
 require 'eve_online/exceptions/timeout_exception'
 require 'eve_online/exceptions/unauthorized_exception'
+require 'eve_online/exceptions/invalid_character_id_exception'
 
 require 'eve_online/base'
 require 'eve_online/base_xml'
