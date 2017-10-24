75537dd912de15a8b53a4d47e946c97e2b0862a5
Igor Zubkov <igor.zubkov@gmail.com>
2017-10-24 13:24:08 +0300

    More specs

--- spec/eve_online/esi/character_attributes_spec.rb
+++ spec/eve_online/esi/character_attributes_spec.rb
@@ -17,10 +17,6 @@ describe EveOnline::ESI::CharacterAttributes do
     its(:character_id) { should eq(12_345_678) }
   end
 
-  # def_delegators :model, :as_json, :charisma, :intelligence, :memory,
-  #                :perception, :willpower, :bonus_remaps,
-  #                :last_remap_date, :accrued_remap_cooldown_date
-
   describe '#model' do
     let(:response) { double }
 
@@ -38,6 +34,96 @@ describe EveOnline::ESI::CharacterAttributes do
     specify { expect { subject.model }.not_to raise_error }
 
     specify { expect { subject.model }.to change { subject.instance_variable_defined?(:@_memoized_model) }.from(false).to(true) }
+  end
+
+  describe '#as_json' do
+    let(:model) { double }
+
+    before { subject.instance_variable_set(:@_memoized_model, model) }
+
+    before { expect(model).to receive(:as_json) }
+
+    specify { expect { subject.as_json }.not_to raise_error }
+  end
+
+  describe '#charisma' do
+    let(:model) { double }
+
+    before { subject.instance_variable_set(:@_memoized_model, model) }
+
+    before { expect(model).to receive(:charisma) }
+
+    specify { expect { subject.charisma }.not_to raise_error }
+  end
+
+  describe '#intelligence' do
+    let(:model) { double }
+
+    before { subject.instance_variable_set(:@_memoized_model, model) }
+
+    before { expect(model).to receive(:intelligence) }
+
+    specify { expect { subject.intelligence }.not_to raise_error }
+  end
+
+  describe '#memory' do
+    let(:model) { double }
+
+    before { subject.instance_variable_set(:@_memoized_model, model) }
+
+    before { expect(model).to receive(:memory) }
+
+    specify { expect { subject.memory }.not_to raise_error }
+  end
+
+  describe '#perception' do
+    let(:model) { double }
+
+    before { subject.instance_variable_set(:@_memoized_model, model) }
+
+    before { expect(model).to receive(:perception) }
+
+    specify { expect { subject.perception }.not_to raise_error }
+  end
+
+  describe '#willpower' do
+    let(:model) { double }
+
+    before { subject.instance_variable_set(:@_memoized_model, model) }
+
+    before { expect(model).to receive(:willpower) }
+
+    specify { expect { subject.willpower }.not_to raise_error }
+  end
+
+  describe '#bonus_remaps' do
+    let(:model) { double }
+
+    before { subject.instance_variable_set(:@_memoized_model, model) }
+
+    before { expect(model).to receive(:bonus_remaps) }
+
+    specify { expect { subject.bonus_remaps }.not_to raise_error }
+  end
+
+  describe '#last_remap_date' do
+    let(:model) { double }
+
+    before { subject.instance_variable_set(:@_memoized_model, model) }
+
+    before { expect(model).to receive(:last_remap_date) }
+
+    specify { expect { subject.last_remap_date }.not_to raise_error }
+  end
+
+  describe '#accrued_remap_cooldown_date' do
+    let(:model) { double }
+
+    before { subject.instance_variable_set(:@_memoized_model, model) }
+
+    before { expect(model).to receive(:accrued_remap_cooldown_date) }
+
+    specify { expect { subject.accrued_remap_cooldown_date }.not_to raise_error }
   end
 
   describe '#scope' do
