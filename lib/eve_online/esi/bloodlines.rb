63d722ca7a6f1d1d8ac2bc6dc9faaf41f9632d44
Igor Zubkov <igor.zubkov@gmail.com>
2017-10-30 20:56:16 +0200

    EveOnline::ESI::Bloodlines

---
+++ lib/eve_online/esi/bloodlines.rb
@@ -0,0 +1,22 @@
+module EveOnline
+  module ESI
+    class Bloodlines < Base
+      API_ENDPOINT = 'https://esi.tech.ccp.is/v1/universe/bloodlines/?datasource=tranquility&language=en-us'.freeze
+
+      def bloodlines
+        output = []
+        response.each do |bloodline|
+          output << Models::Bloodline.new(bloodline)
+        end
+        output
+      end
+      memoize :bloodlines
+
+      def scope; end
+
+      def url
+        API_ENDPOINT
+      end
+    end
+  end
+end
