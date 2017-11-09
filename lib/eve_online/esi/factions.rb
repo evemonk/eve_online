a6ea33b0872769a65ade4cfbe28862f8f6215121
Igor Zubkov <igor.zubkov@gmail.com>
2017-11-09 15:07:57 +0200

    EveOnline::ESI::Factions

---
+++ lib/eve_online/esi/factions.rb
@@ -0,0 +1,22 @@
+module EveOnline
+  module ESI
+    class Factions < Base
+      API_ENDPOINT = 'https://esi.tech.ccp.is/v2/universe/factions/?datasource=tranquility&language=en-us'.freeze
+
+      def factions
+        output = []
+        response.each do |faction|
+          output << Models::Faction.new(faction)
+        end
+        output
+      end
+      memoize :factions
+
+      def scope; end
+
+      def url
+        API_ENDPOINT
+      end
+    end
+  end
+end
