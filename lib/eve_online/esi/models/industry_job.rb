5cabe2c9b57f87e9ce55750fe7e325ab75cdb873
Igor Zubkov <igor.zubkov@gmail.com>
2017-11-20 11:40:53 +0200

    Remove whitespaces

--- lib/eve_online/esi/models/industry_job.rb
+++ lib/eve_online/esi/models/industry_job.rb
@@ -25,7 +25,7 @@ module EveOnline
             start_date: start_date,
             station_id: station_id,
             status: status,
-            successful_runs: successful_runs            
+            successful_runs: successful_runs
           }
         end
 
@@ -56,7 +56,7 @@ module EveOnline
         end
 
         def cost
-          options['cost']          
+          options['cost']
         end
 
         def duration
@@ -96,7 +96,7 @@ module EveOnline
         end
 
         def probability
-           options['probability']         
+           options['probability']
         end
 
         def product_type_id
