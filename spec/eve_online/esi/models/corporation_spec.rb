475d763c9bc2448ff3240de4662e6cdebe9299d3
Igor Zubkov <igor.zubkov@gmail.com>
2017-12-19 12:55:29 +0200

    Update spec

--- spec/eve_online/esi/models/corporation_spec.rb
+++ spec/eve_online/esi/models/corporation_spec.rb
@@ -11,61 +11,69 @@ describe EveOnline::ESI::Models::Corporation do
     its(:options) { should eq(options) }
   end
 
-  # describe '#as_json' do
-  #   let(:corporation) { described_class.new(options) }
-  #
-  #   let(:corporation_description) { double }
-  #
-  #   let(:creation_date) { double }
-  #
-  #   let(:faction) { double }
-  #
-  #   before { expect(corporation).to receive(:alliance_id).and_return(99_001_258) }
-  #
-  #   before { expect(corporation).to receive(:ceo_id).and_return(1_721_864_142) }
-  #
-  #   before { expect(corporation).to receive(:corporation_description).and_return(corporation_description) }
-  #
-  #   before { expect(corporation).to receive(:corporation_name).and_return('Bullshit Bingo Club') }
-  #
-  #   before { expect(corporation).to receive(:creation_date).and_return(creation_date) }
-  #
-  #   before { expect(corporation).to receive(:creator_id).and_return(1_721_864_142) }
-  #
-  #   before { expect(corporation).to receive(:faction).and_return(faction) }
-  #
-  #   before { expect(corporation).to receive(:member_count).and_return(38) }
-  #
-  #   before { expect(corporation).to receive(:tax_rate).and_return(0.1) }
-  #
-  #   before { expect(corporation).to receive(:ticker).and_return('BUBIC') }
-  #
-  #   before { expect(corporation).to receive(:corporation_url).and_return('http://') }
-  #
-  #   subject { corporation.as_json }
-  #
-  #   its([:alliance_id]) { should eq(99_001_258) }
-  #
-  #   its([:ceo_id]) { should eq(1_721_864_142) }
-  #
-  #   its([:corporation_description]) { should eq(corporation_description) }
-  #
-  #   its([:corporation_name]) { should eq('Bullshit Bingo Club') }
-  #
-  #   its([:creation_date]) { should eq(creation_date) }
-  #
-  #   its([:creator_id]) { should eq(1_721_864_142) }
-  #
-  #   its([:faction]) { should eq(faction) }
-  #
-  #   its([:member_count]) { should eq(38) }
-  #
-  #   its([:tax_rate]) { should eq(0.1) }
-  #
-  #   its([:ticker]) { should eq('BUBIC') }
-  #
-  #   its([:corporation_url]) { should eq('http://') }
-  # end
+  describe '#as_json' do
+    let(:corporation) { described_class.new(options) }
+
+    let(:description) { double }
+
+    let(:date_founded) { double }
+
+    let(:faction_id) { double }
+
+    before { expect(corporation).to receive(:name).and_return('Bullshit Bingo Club') }
+
+    before { expect(corporation).to receive(:ticker).and_return('BUBIC') }
+
+    before { expect(corporation).to receive(:member_count).and_return(60) }
+
+    before { expect(corporation).to receive(:ceo_id).and_return(1_721_864_142) }
+
+    before { expect(corporation).to receive(:tax_rate).and_return(0.1) }
+
+    before { expect(corporation).to receive(:creator_id).and_return(1_721_864_142) }
+
+    before { expect(corporation).to receive(:alliance_id).and_return(99_001_258) }
+
+    before { expect(corporation).to receive(:description).and_return(description) }
+
+    before { expect(corporation).to receive(:date_founded).and_return(date_founded) }
+
+    before { expect(corporation).to receive(:corporation_url).and_return('http://') }
+
+    before { expect(corporation).to receive(:faction_id).and_return(faction_id) }
+
+    before { expect(corporation).to receive(:home_station_id).and_return(60_011_893) }
+
+    before { expect(corporation).to receive(:shares).and_return(1000) }
+
+    subject { corporation.as_json }
+
+    its([:name]) { should eq('Bullshit Bingo Club') }
+
+    its([:ticker]) { should eq('BUBIC') }
+
+    its([:member_count]) { should eq(60) }
+
+    its([:ceo_id]) { should eq(1_721_864_142) }
+
+    its([:tax_rate]) { should eq(0.1) }
+
+    its([:creator_id]) { should eq(1_721_864_142) }
+
+    its([:alliance_id]) { should eq(99_001_258) }
+
+    its([:description]) { should eq(description) }
+
+    its([:date_founded]) { should eq(date_founded) }
+
+    its([:corporation_url]) { should eq('http://') }
+
+    its([:faction_id]) { should eq(faction_id) }
+
+    its([:home_station_id]) { should eq(60_011_893) }
+
+    its([:shares]) { should eq(1000) }
+  end
 
   describe '#name' do
     before { expect(options).to receive(:[]).with('name') }
