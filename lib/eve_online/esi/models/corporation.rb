43bb2159ebd2ed58d826851cc2c97d929bf7ca65
Igor Zubkov <igor.zubkov@gmail.com>
2017-12-19 12:14:55 +0200

    Update EveOnline::ESI::Models::Corporation

--- lib/eve_online/esi/models/corporation.rb
+++ lib/eve_online/esi/models/corporation.rb
@@ -4,64 +4,74 @@ module EveOnline
       class Corporation < Base
         def as_json
           {
-            alliance_id: alliance_id,
-            ceo_id: ceo_id,
-            corporation_description: corporation_description,
-            corporation_name: corporation_name,
-            creation_date: creation_date,
-            creator_id: creator_id,
-            faction: faction,
-            member_count: member_count,
-            tax_rate: tax_rate,
+            name: name,
             ticker: ticker,
-            corporation_url: corporation_url
+            member_count: member_count,
+            ceo_id: ceo_id,
+            alliance_id: alliance_id,
+            description: description,
+            tax_rate: tax_rate,
+            date_founded: date_founded,
+            creator_id: creator_id,
+            corporation_url: corporation_url,
+            faction_id: faction_id,
+            home_station_id: home_station_id,
+            shares: shares
           }
         end
 
-        def alliance_id
-          options['alliance_id']
-        end
-
-        def ceo_id
-          options['ceo_id']
-        end
-
-        def corporation_description
-          options['corporation_description']
-        end
-
-        def corporation_name
-          options['corporation_name']
-        end
-
-        def creation_date
-          creation_date = options['creation_date']
-
-          parse_datetime_with_timezone(creation_date) if creation_date
-        end
-
-        def creator_id
-          options['creator_id']
-        end
-
-        def faction
-          options['faction']
-        end
-
-        def member_count
-          options['member_count']
-        end
-
-        def tax_rate
-          options['tax_rate']
+        def name
+          options['name']
         end
 
         def ticker
           options['ticker']
         end
 
+        def member_count
+          options['member_count']
+        end
+
+        def ceo_id
+          options['ceo_id']
+        end
+
+        def alliance_id
+          options['alliance_id']
+        end
+
+        def description
+          options['description']
+        end
+
+        def tax_rate
+          options['tax_rate']
+        end
+
+        def date_founded
+          date_founded = options['date_founded']
+
+          parse_datetime_with_timezone(date_founded) if date_founded
+        end
+
+        def creator_id
+          options['creator_id']
+        end
+
         def corporation_url
           options['url']
+        end
+
+        def home_station_id
+          options['home_station_id']
+        end
+
+        def faction_id
+          options['faction_id']
+        end
+
+        def shares
+          options['shares']
         end
       end
     end
