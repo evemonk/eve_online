676c27849f37fd53866d7822ee28b5ef4181d4bc
Igor Zubkov <igor.zubkov@gmail.com>
2017-12-25 13:13:51 +0200

    EveOnline::ESI::AlliancesNames

--- lib/eve_online.rb
+++ lib/eve_online.rb
@@ -55,6 +55,7 @@ require 'eve_online/esi/alliances'
 require 'eve_online/esi/alliance'
 require 'eve_online/esi/alliance_corporations'
 require 'eve_online/esi/alliance_icon'
+require 'eve_online/esi/alliances_names'
 
 require 'eve_online/esi/dogma_attributes'
 require 'eve_online/esi/dogma_attribute'
