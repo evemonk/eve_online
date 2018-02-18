efb6672778c6b8aa08060e7345aaa9f88991c56b
Igor Zubkov <igor.zubkov@gmail.com>
2018-02-18 23:23:06 +0200

    Add character info feature spec

---
+++ spec/features/character_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "Get character's public information" do
+  let(:options) { { character_id: 1_337_512_245 } }
+
+  before { VCR.insert_cassette 'esi/character/1337512245' }
+
+  after { VCR.eject_cassette }
+
+  subject { EveOnline::ESI::Character.new(options) }
+
+  specify do
+    expect(subject.as_json).to eq(corporation_id: 98_134_807,
+                                  birthday: '2010-01-15T15:26:00Z',
+                                  name: 'Johnn Dillinger',
+                                  gender: 'male',
+                                  race_id: 2,
+                                  bloodline_id: 4,
+                                  description: '',
+                                  alliance_id: 99_005_443,
+                                  ancestry_id: 24,
+                                  security_status: 1.8694881661345457,
+                                  faction_id: nil)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,11 @@ require 'eve_online'
 require 'rspec/its'
 
 Time.zone = 'UTC'
+
+# vcr
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
