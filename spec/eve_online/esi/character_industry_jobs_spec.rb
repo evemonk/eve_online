eb2207e72e50fdb63c912b785ed6910d28b12dc1
Igor Zubkov <igor.zubkov@gmail.com>
2018-02-18 22:37:13 +0200

    Fix rubocop warnings

--- spec/eve_online/esi/character_industry_jobs_spec.rb
+++ spec/eve_online/esi/character_industry_jobs_spec.rb
@@ -7,7 +7,7 @@ describe EveOnline::ESI::CharacterIndustryJobs do
 
   specify { expect(subject).to be_a(EveOnline::ESI::Base) }
 
-  specify { expect(described_class::API_ENDPOINT).to eq('https://esi.tech.ccp.is/v1/characters/%s/industry/jobs/?datasource=tranquility&include_completed=%s') }
+  specify { expect(described_class::API_ENDPOINT).to eq('https://esi.tech.ccp.is/v1/characters/%<character_id>s/industry/jobs/?datasource=tranquility&include_completed=%<include_completed>s') }
 
   describe '#initialize' do
     its(:token) { should eq('token123') }
