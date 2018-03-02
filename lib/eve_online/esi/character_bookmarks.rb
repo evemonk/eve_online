21cfbcab688d53c227af7add97dc662b4665f94c
Igor Zubkov <igor.zubkov@gmail.com>
2018-03-02 17:59:57 +0200

    Add EveOnline::ESI::CharacterBookmarks

---
+++ lib/eve_online/esi/character_bookmarks.rb
@@ -0,0 +1,32 @@
+module EveOnline
+  module ESI
+    class CharacterBookmarks < Base
+      API_ENDPOINT = 'https://esi.tech.ccp.is/v2/characters/%<character_id>s/bookmarks/?datasource=tranquility'.freeze
+
+      attr_reader :character_id
+
+      def initialize(options)
+        super
+
+        @character_id = options[:character_id]
+      end
+
+      # def bookmarks
+      #   output = []
+      #   response.each do |bookmark|
+      #     output << Models::Bookmark.new(bookmark)
+      #   end
+      #   output
+      # end
+      # memoize :bookmarks
+
+      def scope
+        'esi-bookmarks.read_character_bookmarks.v1'
+      end
+
+      def url
+        format(API_ENDPOINT, character_id: character_id)
+      end
+    end
+  end
+end
---
+++ spec/eve_online/esi/character_bookmarks_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe EveOnline::ESI::CharacterBookmarks do
+  let(:options) { { token: 'token123', character_id: 12_345_678 } }
+
+  subject { described_class.new(options) }
+
+  specify { expect(subject).to be_a(EveOnline::ESI::Base) }
+
+  specify { expect(described_class::API_ENDPOINT).to eq('https://esi.tech.ccp.is/v2/characters/%<character_id>s/bookmarks/?datasource=tranquility') }
+
+  describe '#initialize' do
+    its(:token) { should eq('token123') }
+
+    its(:parser) { should eq(JSON) }
+
+    its(:character_id) { should eq(12_345_678) }
+  end
+
+  # describe '#bookmarks' # TODO: write this
+
+  describe '#scope' do
+    specify { expect(subject.scope).to eq('esi-bookmarks.read_character_bookmarks.v1') }
+  end
+
+  describe '#url' do
+    specify do
+      expect(subject.url).to eq('https://esi.tech.ccp.is/v2/characters/12345678/bookmarks/?datasource=tranquility')
+    end
+  end
+end
