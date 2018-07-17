705fe5fd7e2b04c357b1f7d41aad1aa750a24485
Igor Zubkov <igor.zubkov@gmail.com>
2018-07-17 15:13:53 +0300

    Sync with new ESI API

--- lib/eve_online/esi/models/bookmark.rb
+++ lib/eve_online/esi/models/bookmark.rb
@@ -7,56 +7,22 @@ module EveOnline
         def as_json
           {
             bookmark_id: bookmark_id,
-            folder_id: folder_id,
-            created: created,
-            label: label,
-            notes: notes,
-            location_id: location_id,
-            creator_id: creator_id,
-            item_id: item_id,
-            item_type_id: item_type_id,
             coordinate_x: coordinate_x,
             coordinate_y: coordinate_y,
-            coordinate_z: coordinate_z
+            coordinate_z: coordinate_z,
+            created: created,
+            creator_id: creator_id,
+            folder_id: folder_id,
+            item_id: item_id,
+            item_type_id: item_type_id,
+            label: label,
+            location_id: location_id,
+            notes: notes
           }
         end
 
         def bookmark_id
           options['bookmark_id']
-        end
-
-        def folder_id
-          options['folder_id']
-        end
-
-        def created
-          created = options['created']
-
-          parse_datetime_with_timezone(created) if created
-        end
-
-        def label
-          options['label']
-        end
-
-        def notes
-          options['notes']
-        end
-
-        def location_id
-          options['location_id']
-        end
-
-        def creator_id
-          options['creator_id']
-        end
-
-        def item_id
-          options['item']['item_id'] if options['item']
-        end
-
-        def item_type_id
-          options['item']['type_id'] if options['item']
         end
 
         def coordinate_x
@@ -69,6 +35,40 @@ module EveOnline
 
         def coordinate_z
           options['coordinates']['z'] if options['coordinates']
+        end
+
+        def created
+          created = options['created']
+
+          parse_datetime_with_timezone(created) if created
+        end
+
+        def creator_id
+          options['creator_id']
+        end
+
+        def folder_id
+          options['folder_id']
+        end
+
+        def item_id
+          options['item']['item_id'] if options['item']
+        end
+
+        def item_type_id
+          options['item']['type_id'] if options['item']
+        end
+
+        def label
+          options['label']
+        end
+
+        def location_id
+          options['location_id']
+        end
+
+        def notes
+          options['notes']
         end
       end
     end
