3e05c97333c3cf42bb3fcde41f692342aca7ed21
Igor Zubkov <igor.zubkov@gmail.com>
2018-07-17 18:45:59 +0300

    Sync with new ESI API

--- lib/eve_online/esi/models/character.rb
+++ lib/eve_online/esi/models/character.rb
@@ -6,48 +6,18 @@ module EveOnline
       class Character < Base
         def as_json
           {
-            corporation_id: corporation_id,
-            birthday: birthday,
-            name: name,
-            gender: gender,
-            race_id: race_id,
-            bloodline_id: bloodline_id,
-            description: description,
             alliance_id: alliance_id,
             ancestry_id: ancestry_id,
-            security_status: security_status,
-            faction_id: faction_id
+            birthday: birthday,
+            bloodline_id: bloodline_id,
+            corporation_id: corporation_id,
+            description: description,
+            faction_id: faction_id,
+            gender: gender,
+            name: name,
+            race_id: race_id,
+            security_status: security_status
           }
-        end
-
-        def corporation_id
-          options['corporation_id']
-        end
-
-        def birthday
-          birthday = options['birthday']
-
-          parse_datetime_with_timezone(birthday) if birthday
-        end
-
-        def name
-          options['name']
-        end
-
-        def gender
-          options['gender']
-        end
-
-        def race_id
-          options['race_id']
-        end
-
-        def bloodline_id
-          options['bloodline_id']
-        end
-
-        def description
-          options['description']
         end
 
         def alliance_id
@@ -58,12 +28,42 @@ module EveOnline
           options['ancestry_id']
         end
 
-        def security_status
-          options['security_status']
+        def birthday
+          birthday = options['birthday']
+
+          parse_datetime_with_timezone(birthday) if birthday
+        end
+
+        def bloodline_id
+          options['bloodline_id']
+        end
+
+        def corporation_id
+          options['corporation_id']
+        end
+
+        def description
+          options['description']
         end
 
         def faction_id
           options['faction_id']
+        end
+
+        def gender
+          options['gender']
+        end
+
+        def name
+          options['name']
+        end
+
+        def race_id
+          options['race_id']
+        end
+
+        def security_status
+          options['security_status']
         end
       end
     end
