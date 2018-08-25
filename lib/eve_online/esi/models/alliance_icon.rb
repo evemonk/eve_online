44350ed333cc8799e2ab44979197bea8a43925e6
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-25 23:07:16 +0300

    Sync with new ESI API

--- lib/eve_online/esi/models/alliance_icon.rb
+++ lib/eve_online/esi/models/alliance_icon.rb
@@ -6,17 +6,17 @@ module EveOnline
       class AllianceIcon < Base
         def as_json
           {
+            icon_medium: icon_medium,
             icon_small: icon_small,
-            icon_medium: icon_medium
           }
-        end
-
-        def icon_small
-          options['px64x64']
         end
 
         def icon_medium
           options['px128x128']
+        end
+
+        def icon_small
+          options['px64x64']
         end
       end
     end
