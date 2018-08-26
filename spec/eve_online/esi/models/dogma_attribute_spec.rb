a01a4c7fbfc15b30c376bc35af8c8a5dfc688ea9
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-26 03:07:20 +0300

    Sync with new ESI API

--- spec/eve_online/esi/models/dogma_attribute_spec.rb
+++ spec/eve_online/esi/models/dogma_attribute_spec.rb
@@ -22,53 +22,53 @@ describe EveOnline::ESI::Models::DogmaAttribute do
 
     let(:unit_id) { double }
 
-    before { expect(dogma_attribute).to receive(:id).and_return(2) }
-
-    before { expect(dogma_attribute).to receive(:name).and_return('isOnline') }
-
-    before { expect(dogma_attribute).to receive(:description).and_return('Boolean to store status of online effect') }
-
-    before { expect(dogma_attribute).to receive(:icon_id).and_return(icon_id) }
+    before { expect(dogma_attribute).to receive(:attribute_id).and_return(2) }
 
     before { expect(dogma_attribute).to receive(:default_value).and_return(0.0) }
 
-    before { expect(dogma_attribute).to receive(:published).and_return(published) }
+    before { expect(dogma_attribute).to receive(:description).and_return('Boolean to store status of online effect') }
 
     before { expect(dogma_attribute).to receive(:display_name).and_return('') }
 
-    before { expect(dogma_attribute).to receive(:unit_id).and_return(unit_id) }
+    before { expect(dogma_attribute).to receive(:high_is_good).and_return(true) }
+
+    before { expect(dogma_attribute).to receive(:icon_id).and_return(icon_id) }
+
+    before { expect(dogma_attribute).to receive(:name).and_return('isOnline') }
+
+    before { expect(dogma_attribute).to receive(:published).and_return(published) }
 
     before { expect(dogma_attribute).to receive(:stackable).and_return(true) }
 
-    before { expect(dogma_attribute).to receive(:high_is_good).and_return(true) }
+    before { expect(dogma_attribute).to receive(:unit_id).and_return(unit_id) }
 
     subject { dogma_attribute.as_json }
 
-    its([:id]) { should eq(2) }
-
-    its([:name]) { should eq('isOnline') }
-
-    its([:description]) { should eq('Boolean to store status of online effect') }
-
-    its([:icon_id]) { should eq(icon_id) }
+    its([:attribute_id]) { should eq(2) }
 
     its([:default_value]) { should eq(0.0) }
 
-    its([:published]) { should eq(published) }
+    its([:description]) { should eq('Boolean to store status of online effect') }
 
     its([:display_name]) { should eq('') }
 
-    its([:unit_id]) { should eq(unit_id) }
+    its([:high_is_good]) { should eq(true) }
+
+    its([:icon_id]) { should eq(icon_id) }
+
+    its([:name]) { should eq('isOnline') }
+
+    its([:published]) { should eq(published) }
 
     its([:stackable]) { should eq(true) }
 
-    its([:high_is_good]) { should eq(true) }
+    its([:unit_id]) { should eq(unit_id) }
   end
 
-  describe '#id' do
+  describe '#attribute_id' do
     before { expect(options).to receive(:[]).with('attribute_id') }
 
-    specify { expect { subject.id }.not_to raise_error }
+    specify { expect { subject.attribute_id }.not_to raise_error }
   end
 
   describe '#name' do
