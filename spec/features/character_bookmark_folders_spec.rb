5c07b5f41be6298e8d9d13cddebe0991e0430684
Igor Zubkov <igor.zubkov@gmail.com>
2018-09-11 18:08:33 +0300

    One more spec

---
+++ spec/features/character_bookmark_folders_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Get character bookmark folders' do
+  # TODO: update response json
+  let(:options) do
+    {
+      character_id: 1_337_512_245,
+      token: 'token123',
+      page: 1
+    }
+  end
+
+  before { VCR.insert_cassette 'esi/character_bookmark_folders/1337512245' }
+
+  after { VCR.eject_cassette }
+
+  subject { EveOnline::ESI::CharacterBookmarkFolders.new(options) }
+
+  specify { expect(subject.page).to eq(1) }
+
+  specify { expect(subject.total_pages).to eq(1) }
+
+  specify { expect(subject.bookmark_folders.size).to eq(0) }
+
+  # TODO: test real
+  # specify do
+  #   expect(subject.bookmark_folders.first.as_json).to eq('')
+  # end
+end
