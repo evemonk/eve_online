093e886058af8fc7ee67a427a5e6c61c67c47774
Igor Zubkov <igor.zubkov@gmail.com>
2018-09-12 16:07:09 +0300

    EveOnline::ESI::Models::Region

---
+++ lib/eve_online/esi/models/region.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module EveOnline
+  module ESI
+    module Models
+      class Region < Base
+        def as_json
+          {
+            constellations: constellations,
+            description: description,
+            name: name,
+            region_id: region_id
+          }
+        end
+
+        def constellations
+          options['constellations']
+        end
+
+        def description
+          options['description']
+        end
+
+        def name
+          options['name']
+        end
+
+        def region_id
+          options['region_id']
+        end
+      end
+    end
+  end
+end
---
+++ spec/eve_online/esi/models/region_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe EveOnline::ESI::Models::Region do
+  it { should be_a(EveOnline::ESI::Models::Base) }
+
+  let(:options) { double }
+
+  subject { described_class.new(options) }
+
+  describe '#initialize' do
+    its(:options) { should eq(options) }
+  end
+
+  describe '#as_json' do
+    let(:races) { described_class.new(options) }
+
+    before { expect(races).to receive(:constellations).and_return([20_000_001]) }
+
+    before { expect(races).to receive(:description).and_return('The Derelik region...') }
+
+    before { expect(races).to receive(:name).and_return('Derelik') }
+
+    before { expect(races).to receive(:region_id).and_return(10_000_001) }
+
+    subject { races.as_json }
+
+    its([:constellations]) { should eq([20_000_001]) }
+
+    its([:description]) { should eq('The Derelik region...') }
+
+    its([:name]) { should eq('Derelik') }
+
+    its([:region_id]) { should eq(10_000_001) }
+  end
+
+  describe '#constellations' do
+    before { expect(options).to receive(:[]).with('constellations') }
+
+    specify { expect { subject.constellations }.not_to raise_error }
+  end
+
+  describe '#description' do
+    before { expect(options).to receive(:[]).with('description') }
+
+    specify { expect { subject.description }.not_to raise_error }
+  end
+
+  describe '#name' do
+    before { expect(options).to receive(:[]).with('name') }
+
+    specify { expect { subject.name }.not_to raise_error }
+  end
+
+  describe '#region_id' do
+    before { expect(options).to receive(:[]).with('region_id') }
+
+    specify { expect { subject.region_id }.not_to raise_error }
+  end
+end
