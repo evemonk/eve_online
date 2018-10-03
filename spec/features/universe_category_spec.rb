7dab29d385a67e587f8793dff1803a69c6e31871
Igor Zubkov <igor.zubkov@gmail.com>
2018-10-03 19:31:05 +0300

    More specs

---
+++ spec/features/universe_category_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Get item category information' do
+  before { VCR.insert_cassette 'esi/universe/category/6' }
+
+  after { VCR.eject_cassette }
+
+  let(:options) { { id: 6 } }
+
+  subject { EveOnline::ESI::UniverseCategory.new(options) }
+
+  specify { expect(subject.scope).to eq(nil) }
+
+  specify do
+    expect(subject.as_json).to eq(category_id: 6,
+                                  name: 'Ship',
+                                  published: true)
+  end
+
+  specify { expect(subject.group_ids.size).to eq(46) }
+
+  specify { expect(subject.group_ids.first).to eq(25) }
+end
