339279048fa51d8555119a965c550c734924e1b7
Igor Zubkov <iz@rademade.com>
2018-10-17 14:05:01 +0300

    EveOnline::ESI::Models::Moon

---
+++ lib/eve_online/esi/models/moon.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module EveOnline
+  module ESI
+    module Models
+      class Moon < Base
+        def as_json
+          {
+            moon_id: moon_id,
+            name: name,
+            system_id: system_id
+          }
+        end
+
+        def moon_id
+          options['moon_id']
+        end
+
+        def name
+          options['name']
+        end
+
+        def system_id
+          options['system_id']
+        end
+
+        def position
+          @position ||= Position.new(options['position'])
+        end
+      end
+    end
+  end
+end
