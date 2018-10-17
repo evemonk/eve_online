cfd27db59d8b839ca1086da13f72ddd45ae2f404
Igor Zubkov <igor.zubkov@gmail.com>
2018-10-17 19:53:58 +0300

    EveOnline::ESI::Models::SystemKill

---
+++ lib/eve_online/esi/models/system_kill.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module EveOnline
+  module ESI
+    module Models
+      class SystemKill < Base
+        def as_json
+          {
+            npc_kills: npc_kills,
+            pod_kills: pod_kills,
+            ship_kills: ship_kills,
+            system_id: system_id
+          }
+        end
+
+        def npc_kills
+          options['npc_kills']
+        end
+
+        def pod_kills
+          options['pod_kills']
+        end
+
+        def ship_kills
+          options['ship_kills']
+        end
+
+        def system_id
+          options['system_id']
+        end
+      end
+    end
+  end
+end
---
+++ spec/eve_online/esi/models/system_kill_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe EveOnline::ESI::Models::SystemKill do
+  it { should be_a(EveOnline::ESI::Models::Base) }
+
+  let(:options) { double }
+
+  subject { described_class.new(options) }
+
+  describe '#initialize' do
+    its(:options) { should eq(options) }
+  end
+
+  describe '#as_json' do
+    let(:system_kill) { described_class.new(options) }
+
+    before { expect(system_kill).to receive(:npc_kills).and_return(231) }
+
+    before { expect(system_kill).to receive(:pod_kills).and_return(0) }
+
+    before { expect(system_kill).to receive(:ship_kills).and_return(0) }
+
+    before { expect(system_kill).to receive(:system_id).and_return(30_001_746) }
+
+    subject { system_kill.as_json }
+
+    its([:npc_kills]) { should eq(231) }
+
+    its([:pod_kills]) { should eq(0) }
+
+    its([:ship_kills]) { should eq(0) }
+
+    its([:system_id]) { should eq(30_001_746) }
+  end
+
+  describe '#npc_kills' do
+    before { expect(options).to receive(:[]).with('npc_kills') }
+
+    specify { expect { subject.npc_kills }.not_to raise_error }
+  end
+
+  describe '#pod_kills' do
+    before { expect(options).to receive(:[]).with('pod_kills') }
+
+    specify { expect { subject.pod_kills }.not_to raise_error }
+  end
+
+  describe '#ship_kills' do
+    before { expect(options).to receive(:[]).with('ship_kills') }
+
+    specify { expect { subject.ship_kills }.not_to raise_error }
+  end
+
+  describe '#system_id' do
+    before { expect(options).to receive(:[]).with('system_id') }
+
+    specify { expect { subject.system_id }.not_to raise_error }
+  end
+end
