9a71a1852448df6f29d6c3b0acd2061e00b6269e
Igor Zubkov <igor.zubkov@gmail.com>
2018-10-31 13:56:41 +0200

    Update specs

--- spec/eve_online/esi/character_location_spec.rb
+++ spec/eve_online/esi/character_location_spec.rb
@@ -26,28 +26,38 @@ describe EveOnline::ESI::CharacterLocation do
   end
 
   describe '#model' do
-    let(:response) { double }
+    context 'when @model set' do
+      let(:model) { instance_double(EveOnline::ESI::Models::CharacterLocation) }
 
-    before { expect(subject).to receive(:response).and_return(response) }
+      before { subject.instance_variable_set(:@model, model) }
 
-    let(:model) { instance_double(EveOnline::ESI::Models::CharacterLocation) }
-
-    before do
-      #
-      # EveOnline::ESI::Models::CharacterLocation.new(response) # => model
-      #
-      expect(EveOnline::ESI::Models::CharacterLocation).to receive(:new).with(response).and_return(model)
+      specify { expect(subject.model).to eq(model) }
     end
 
-    specify { expect { subject.model }.not_to raise_error }
+    context 'when @model not set' do
+      let(:response) { double }
 
-    specify { expect { subject.model }.to change { subject.instance_variable_defined?(:@_memoized_model) }.from(false).to(true) }
+      before { expect(subject).to receive(:response).and_return(response) }
+
+      let(:model) { instance_double(EveOnline::ESI::Models::CharacterLocation) }
+
+      before do
+        #
+        # EveOnline::ESI::Models::CharacterLocation.new(response) # => model
+        #
+        expect(EveOnline::ESI::Models::CharacterLocation).to receive(:new).with(response).and_return(model)
+      end
+
+      specify { expect { subject.model }.not_to raise_error }
+
+      specify { expect { subject.model }.to change { subject.instance_variable_get(:@model) }.from(nil).to(model) }
+    end
   end
 
   describe '#as_json' do
     let(:model) { instance_double(EveOnline::ESI::Models::CharacterLocation) }
 
-    before { subject.instance_variable_set(:@_memoized_model, model) }
+    before { subject.instance_variable_set(:@model, model) }
 
     before { expect(model).to receive(:as_json) }
 
@@ -57,7 +67,7 @@ describe EveOnline::ESI::CharacterLocation do
   describe '#solar_system_id' do
     let(:model) { instance_double(EveOnline::ESI::Models::CharacterLocation) }
 
-    before { subject.instance_variable_set(:@_memoized_model, model) }
+    before { subject.instance_variable_set(:@model, model) }
 
     before { expect(model).to receive(:solar_system_id) }
 
@@ -67,7 +77,7 @@ describe EveOnline::ESI::CharacterLocation do
   describe '#station_id' do
     let(:model) { instance_double(EveOnline::ESI::Models::CharacterLocation) }
 
-    before { subject.instance_variable_set(:@_memoized_model, model) }
+    before { subject.instance_variable_set(:@model, model) }
 
     before { expect(model).to receive(:station_id) }
 
@@ -77,7 +87,7 @@ describe EveOnline::ESI::CharacterLocation do
   describe '#structure_id' do
     let(:model) { instance_double(EveOnline::ESI::Models::CharacterLocation) }
 
-    before { subject.instance_variable_set(:@_memoized_model, model) }
+    before { subject.instance_variable_set(:@model, model) }
 
     before { expect(model).to receive(:structure_id) }
 
