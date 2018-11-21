2a5d6a830de617620cffe62c3d0419f781ac097c
Igor Zubkov <igor.zubkov@gmail.com>
2018-11-21 18:21:43 +0200

    More specs

--- spec/features/universe_star_spec.rb
+++ spec/features/universe_star_spec.rb
@@ -3,25 +3,41 @@
 require 'spec_helper'
 
 describe 'Get star information' do
-  before { VCR.insert_cassette 'esi/universe/stars/40000001' }
+  context 'when etag empty' do
+    before { VCR.insert_cassette 'esi/universe/stars/40000001' }
 
-  after { VCR.eject_cassette }
+    after { VCR.eject_cassette }
 
-  let(:options) { { id: 40_000_001 } }
+    let(:options) { { id: 40_000_001 } }
 
-  subject { EveOnline::ESI::UniverseStar.new(options) }
+    subject { EveOnline::ESI::UniverseStar.new(options) }
 
-  specify { expect(subject.scope).to eq(nil) }
+    specify { expect(subject.scope).to eq(nil) }
 
-  specify do
-    expect(subject.as_json).to eq(age: 14_262_808_228,
-                                  luminosity: 0.01575000025331974,
-                                  name: 'Tanoo - Star',
-                                  radius: 126_700_000,
-                                  solar_system_id: 30_000_001,
-                                  spectral_class: 'K2 V',
-                                  temperature: 4567,
-                                  type_id: 45_041,
-                                  etag: 'f5499847d582a932ee279a6db7608659f4ce5b2736df0b8857438717')
+    specify { expect(subject.not_modified?).to eq(false) }
+
+    specify do
+      expect(subject.as_json).to eq(age: 14_262_808_228,
+                                    luminosity: 0.01575000025331974,
+                                    name: 'Tanoo - Star',
+                                    radius: 126_700_000,
+                                    solar_system_id: 30_000_001,
+                                    spectral_class: 'K2 V',
+                                    temperature: 4567,
+                                    type_id: 45_041,
+                                    etag: '2e28835f91024608719726b655591d531125a023e122859d174923d5')
+    end
+  end
+
+  context 'when etag present' do
+    before { VCR.insert_cassette 'esi/universe/stars/40000001_with_etag' }
+
+    after { VCR.eject_cassette }
+
+    let(:options) { { id: 40_000_001, etag: '2e28835f91024608719726b655591d531125a023e122859d174923d5' } }
+
+    subject { EveOnline::ESI::UniverseStar.new(options) }
+
+    specify { expect(subject.not_modified?).to eq(true) }
   end
 end
