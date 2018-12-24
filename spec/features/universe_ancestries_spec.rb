cb47dbfebd7c762c473e4ab75088cc1d0150db2a
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-24 18:42:41 +0200

    More feature specs

---
+++ spec/features/universe_ancestries_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Get ancestries' do
+  context 'when etag not set' do
+    before { VCR.insert_cassette 'esi/universe/ancestries' }
+
+    after { VCR.eject_cassette }
+
+    subject { EveOnline::ESI::UniverseAncestries.new }
+
+    specify { expect(subject.not_modified?).to eq(false) }
+
+    specify { expect(subject.scope).to eq(nil) }
+
+    specify { expect(subject.ancestries.size).to eq(42) }
+
+    specify do
+      expect(subject.ancestries.first.as_json).to eq(ancestry_id: 1,
+                                                     bloodline_id: 5,
+                                                     description: "Holders, the major landholding class in Amarr society, are generally conservative traditionalists. A few, however, have elected to break ranks with their hidebound and power-hungry peers, instead supporting the modernization of their society's religion and substantial economic reform. Their champion is Catiz Tash-Murkon, the Udorian Royal Heir.",
+                                                     icon_id: 1641,
+                                                     name: 'Liberal Holders',
+                                                     short_description: 'Progressive members of the upper class who have rejected their traditional ways.')
+    end
+
+    specify { expect(subject.etag).to eq('e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b') }
+  end
+
+  context 'when etag is set' do
+    before { VCR.insert_cassette 'esi/universe/ancestries_with_etag' }
+
+    after { VCR.eject_cassette }
+
+    subject { EveOnline::ESI::UniverseAncestries.new(etag: 'e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b') }
+
+    specify { expect(subject.not_modified?).to eq(true) }
+
+    specify { expect(subject.etag).to eq('e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b') }
+  end
+end
