70df724ca23aa82a4aab6ae2f1fd54e1093b8082
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-25 21:19:30 +0200

    More feature specs

--- spec/features/alliance_corporations_spec.rb
+++ spec/features/alliance_corporations_spec.rb
@@ -3,17 +3,40 @@
 require 'spec_helper'
 
 describe "List alliance's corporations" do
-  let(:options) { { alliance_id: 99_005_443 } }
+  context 'when etag not set' do
+    let(:options) { { alliance_id: 99_005_443 } }
 
-  before { VCR.insert_cassette 'esi/alliance_corporations/99005443' }
+    before { VCR.insert_cassette 'esi/alliance_corporations/99005443' }
 
-  after { VCR.eject_cassette }
+    after { VCR.eject_cassette }
 
-  subject { EveOnline::ESI::AllianceCorporations.new(options) }
+    subject { EveOnline::ESI::AllianceCorporations.new(options) }
 
-  specify { expect(subject.scope).to eq(nil) }
+    specify { expect(subject.scope).to eq(nil) }
 
-  specify { expect(subject.corporation_ids.size).to eq(67) }
+    specify { expect(subject.corporation_ids.size).to eq(70) }
 
-  specify { expect(subject.corporation_ids.first).to eq(98_134_807) }
+    specify { expect(subject.corporation_ids.first).to eq(98_091_533) }
+
+    specify { expect(subject.etag).to eq('9b6810d8ddbd9a13bb4ad09cf4bfefd9f29974bffd543eb30ce25132') }
+  end
+
+  context 'when etag is set' do
+    let(:options) do
+      {
+        alliance_id: 99_005_443,
+        etag: '9b6810d8ddbd9a13bb4ad09cf4bfefd9f29974bffd543eb30ce25132'
+      }
+    end
+
+    before { VCR.insert_cassette 'esi/alliance_corporations/99005443_with_etag' }
+
+    after { VCR.eject_cassette }
+
+    subject { EveOnline::ESI::AllianceCorporations.new(options) }
+
+    specify { expect(subject.not_modified?).to eq(true) }
+
+    specify { expect(subject.etag).to eq('9b6810d8ddbd9a13bb4ad09cf4bfefd9f29974bffd543eb30ce25132') }
+  end
 end
