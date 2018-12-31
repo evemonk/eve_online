a577c8c01596b2722f0b8db02b02ec0b5a1261a2
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-01 00:34:42 +0200

    More specs

--- spec/features/character_spec.rb
+++ spec/features/character_spec.rb
@@ -3,27 +3,50 @@
 require 'spec_helper'
 
 describe "Get character's public information" do
-  let(:options) { { character_id: 1_337_512_245 } }
+  context 'when etag not set' do
+    let(:options) { { character_id: 1_337_512_245 } }
 
-  before { VCR.insert_cassette 'esi/characters/1337512245' }
+    before { VCR.insert_cassette 'esi/characters/1337512245' }
 
-  after { VCR.eject_cassette }
+    after { VCR.eject_cassette }
 
-  subject { EveOnline::ESI::Character.new(options) }
+    subject { EveOnline::ESI::Character.new(options) }
 
-  specify { expect(subject.scope).to eq(nil) }
+    specify { expect(subject.scope).to eq(nil) }
 
-  specify do
-    expect(subject.as_json).to eq(corporation_id: 98_134_807,
-                                  birthday: '2010-01-15T15:26:00Z',
-                                  name: 'Johnn Dillinger',
-                                  gender: 'male',
-                                  race_id: 2,
-                                  bloodline_id: 4,
-                                  description: '',
-                                  alliance_id: 99_005_443,
-                                  ancestry_id: 24,
-                                  security_status: 1.8694881661345457,
-                                  faction_id: nil)
+    specify do
+      expect(subject.as_json).to eq(corporation_id: 98_134_807,
+                                    birthday: '2010-01-15T15:26:00Z',
+                                    name: 'Johnn Dillinger',
+                                    gender: 'male',
+                                    race_id: 2,
+                                    bloodline_id: 4,
+                                    description: '',
+                                    alliance_id: 99_005_443,
+                                    ancestry_id: 24,
+                                    security_status: 1.8694881661345457,
+                                    faction_id: nil)
+    end
+
+    specify { expect(subject.etag).to eq('22c39689783a86032b8d43fa0b2e8f4809c4f38a585e39471035aa8b') }
+  end
+
+  context 'when etag is set' do
+    let(:options) do
+      {
+        character_id: 1_337_512_245,
+        etag: '22c39689783a86032b8d43fa0b2e8f4809c4f38a585e39471035aa8b'
+      }
+    end
+
+    before { VCR.insert_cassette 'esi/characters/1337512245_with_etag' }
+
+    after { VCR.eject_cassette }
+
+    subject { EveOnline::ESI::Character.new(options) }
+
+    specify { expect(subject.not_modified?).to eq(true) }
+
+    specify { expect(subject.etag).to eq('22c39689783a86032b8d43fa0b2e8f4809c4f38a585e39471035aa8b') }
   end
 end
