d3a15113bca098d7df03a419f9980e9240c455af
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-16 19:10:12 +0200

    More feature specs

---
+++ spec/features/dogma_attributes_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Get attributes' do
+  context 'when etag not set' do
+    before { VCR.insert_cassette 'esi/dogma/attributes' }
+
+    after { VCR.eject_cassette }
+
+    subject { EveOnline::ESI::DogmaAttributes.new }
+
+    specify { expect(subject.scope).to eq(nil) }
+
+    specify { expect(subject.not_modified?).to eq(false) }
+
+    specify { expect(subject.attribute_ids.size).to eq(2463) }
+
+    specify { expect(subject.attribute_ids.first).to eq(2) }
+
+    specify { expect(subject.etag).to eq('5c9218218aca123ef8c106f6607bfe8e6e086d2fc2b972bbd8ff03d2') }
+  end
+end
