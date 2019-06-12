52515c7b92068a07fa28fd4eaaa988906bf20094
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-13 00:53:21 +0300

    One more spec

--- spec/features/corporation_loyalty_store_offers_spec.rb
+++ spec/features/corporation_loyalty_store_offers_spec.rb
@@ -14,6 +14,8 @@ describe 'List loyalty store offers' do
 
     specify { expect(subject.scope).to eq(nil) }
 
+    specify { expect(subject.roles).to eq([]) }
+
     specify { expect(subject.not_modified?).to eq(false) }
 
     specify { expect(subject.offers.size).to eq(332) }
