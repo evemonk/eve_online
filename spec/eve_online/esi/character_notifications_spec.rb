08643622a473e54fd8396afc2a7d1605965ea1be
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-13 17:19:25 +0300

    New hash syntax

--- spec/eve_online/esi/character_notifications_spec.rb
+++ spec/eve_online/esi/character_notifications_spec.rb
@@ -40,12 +40,12 @@ describe EveOnline::ESI::CharacterNotifications do
       let(:response) do
         [
           {
-            'notification_id' => 774_328_832,
-            'type' => 'AllWarDeclaredMsg',
-            'sender_id' => 1_000_125,
-            'sender_type' => 'corporation',
-            'timestamp' => '2018-03-01T13:48:00Z',
-            'text' => 'againstID: 99005443\ncost: 0\ndeclaredByID: 98442842\ndelayHours: 24\nhostileState: 0\n'
+            notification_id: 774_328_832,
+            type: 'AllWarDeclaredMsg',
+            sender_id: 1_000_125,
+            sender_type: 'corporation',
+            timestamp: '2018-03-01T13:48:00Z',
+            text: 'againstID: 99005443\ncost: 0\ndeclaredByID: 98442842\ndelayHours: 24\nhostileState: 0\n'
           }
         ]
       end
