99d8117b31e534db086995e5d79d344f780bea0b
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-14 19:50:04 +0300

    New hash syntax

--- spec/eve_online/esi/corporation_blueprints_spec.rb
+++ spec/eve_online/esi/corporation_blueprints_spec.rb
@@ -50,14 +50,14 @@ describe EveOnline::ESI::CorporationBlueprints do
       let(:response) do
         [
           {
-            'item_id' => 1_000_000_010_495,
-            'type_id' => 691,
-            'location_id' => 60_014_719,
-            'location_flag' => 'CorpSAG1',
-            'quantity' => 1,
-            'time_efficiency' => 0,
-            'material_efficiency' => 0,
-            'runs' => -1
+            item_id: 1_000_000_010_495,
+            type_id: 691,
+            location_id: 60_014_719,
+            location_flag: 'CorpSAG1',
+            quantity: 1,
+            time_efficiency: 0,
+            material_efficiency: 0,
+            runs: -1
           }
         ]
       end
