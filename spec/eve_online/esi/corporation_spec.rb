c3c8c9c7ddda89b2027f2ae0496704f69ba96545
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-02 16:07:23 +0300

    Update EveOnline::ESI::Corporation

--- spec/eve_online/esi/corporation_spec.rb
+++ spec/eve_online/esi/corporation_spec.rb
@@ -9,7 +9,7 @@ describe EveOnline::ESI::Corporation do
 
   specify { expect(subject).to be_a(EveOnline::ESI::Base) }
 
-  specify { expect(described_class::API_PATH).to eq('/v4/corporations/%<corporation_id>s/?datasource=%<datasource>s') }
+  specify { expect(described_class::API_PATH).to eq('/v4/corporations/%<corporation_id>s/') }
 
   describe '#initialize' do
     its(:token) { should eq(nil) }
@@ -210,6 +210,18 @@ describe EveOnline::ESI::Corporation do
 
   describe '#scope' do
     specify { expect(subject.scope).to eq(nil) }
+  end
+
+  describe '#path' do
+    specify do
+      expect(subject.path).to eq('/v4/corporations/12345678/')
+    end
+  end
+
+  describe '#query' do
+    specify do
+      expect(subject.query).to eq(datasource: 'tranquility')
+    end
   end
 
   describe '#url' do
