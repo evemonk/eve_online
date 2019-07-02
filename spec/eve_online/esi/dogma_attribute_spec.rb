f4f0052c55da8659b25932ce5674fd498ebc9cac
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-02 16:09:47 +0300

    Update EveOnline::ESI::DogmaAttribute

--- spec/eve_online/esi/dogma_attribute_spec.rb
+++ spec/eve_online/esi/dogma_attribute_spec.rb
@@ -9,7 +9,7 @@ describe EveOnline::ESI::DogmaAttribute do
 
   specify { expect(subject).to be_a(EveOnline::ESI::Base) }
 
-  specify { expect(described_class::API_PATH).to eq('/v1/dogma/attributes/%<attribute_id>s/?datasource=%<datasource>s') }
+  specify { expect(described_class::API_PATH).to eq('/v1/dogma/attributes/%<attribute_id>s/') }
 
   describe '#initialize' do
     its(:token) { should eq(nil) }
@@ -170,6 +170,18 @@ describe EveOnline::ESI::DogmaAttribute do
 
   describe '#scope' do
     specify { expect(subject.scope).to eq(nil) }
+  end
+
+  describe '#path' do
+    specify do
+      expect(subject.path).to eq('/v1/dogma/attributes/2/')
+    end
+  end
+
+  describe '#query' do
+    specify do
+      expect(subject.query).to eq(datasource: 'tranquility')
+    end
   end
 
   describe '#url' do
