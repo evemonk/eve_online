15a2168efb80b3ebfb39dfc58ae0700e6f4300e6
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-02 13:50:18 +0300

    Update EveOnline::ESI::CharacterFatigue

--- spec/eve_online/esi/character_fatigue_spec.rb
+++ spec/eve_online/esi/character_fatigue_spec.rb
@@ -9,7 +9,7 @@ describe EveOnline::ESI::CharacterFatigue do
 
   specify { expect(subject).to be_a(EveOnline::ESI::Base) }
 
-  specify { expect(described_class::API_PATH).to eq('/v1/characters/%<character_id>s/fatigue/?datasource=%<datasource>s') }
+  specify { expect(described_class::API_PATH).to eq('/v1/characters/%<character_id>s/fatigue/') }
 
   describe '#initialize' do
     its(:token) { should eq('token123') }
@@ -101,6 +101,18 @@ describe EveOnline::ESI::CharacterFatigue do
 
   describe '#scope' do
     specify { expect(subject.scope).to eq('esi-characters.read_fatigue.v1') }
+  end
+
+  describe '#path' do
+    specify do
+      expect(subject.path).to eq('/v1/characters/12345678/fatigue/')
+    end
+  end
+
+  describe '#query' do
+    specify do
+      expect(subject.query).to eq(datasource: 'tranquility')
+    end
   end
 
   describe '#url' do
