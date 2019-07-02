3058758f7c9afe6cb5c8a03d60f1eaab776b7661
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-03 00:37:18 +0300

    Update feature spec

--- spec/features/universe_structures_spec.rb
+++ spec/features/universe_structures_spec.rb
@@ -14,11 +14,11 @@ describe 'List all public structures' do
 
     specify { expect(subject.not_modified?).to eq(false) }
 
-    specify { expect(subject.structure_ids.size).to eq(4004) }
+    specify { expect(subject.structure_ids.size).to eq(3654) }
 
     specify { expect(subject.structure_ids.first).to eq(1_027_528_548_355) }
 
-    specify { expect(subject.etag).to eq('6edc271fcf967ee50bec6935856501b233e58790fad237b097946a4c') }
+    specify { expect(subject.etag).to eq('1b64f9a8268bb3ba350acc7d7116bfd75dd9f88defb1bbc61771aec7') }
   end
 
   context 'when etag is set' do
@@ -26,10 +26,10 @@ describe 'List all public structures' do
 
     after { VCR.eject_cassette }
 
-    subject { EveOnline::ESI::UniverseStructures.new(etag: '6edc271fcf967ee50bec6935856501b233e58790fad237b097946a4c') }
+    subject { EveOnline::ESI::UniverseStructures.new(etag: '1b64f9a8268bb3ba350acc7d7116bfd75dd9f88defb1bbc61771aec7') }
 
     specify { expect(subject.not_modified?).to eq(true) }
 
-    specify { expect(subject.etag).to eq('6edc271fcf967ee50bec6935856501b233e58790fad237b097946a4c') }
+    specify { expect(subject.etag).to eq('1b64f9a8268bb3ba350acc7d7116bfd75dd9f88defb1bbc61771aec7') }
   end
 end
