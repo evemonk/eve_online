470359277c6d9c3683379bd9e7291081a8502340
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-28 15:59:01 +0200

    Add spec

---
+++ spec/features/war_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Get war information' do
+  context 'when etag not set' do
+
+  end
+
+  context 'when etag is set' do
+
+  end
+end
