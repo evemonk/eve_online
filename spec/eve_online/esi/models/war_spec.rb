9d423bfbd5059489f26aadd21f681330e4eeb478
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-28 15:53:10 +0200

    More specs

--- spec/eve_online/esi/models/war_spec.rb
+++ spec/eve_online/esi/models/war_spec.rb
@@ -20,6 +20,10 @@ describe EveOnline::ESI::Models::War do
 
     let(:finished) { double }
 
+    let(:mutual) { double }
+
+    let(:open_for_allies) { double }
+
     let(:retracted) { double }
 
     let(:started) { double }
@@ -30,9 +34,9 @@ describe EveOnline::ESI::Models::War do
 
     before { expect(war).to receive(:war_id).and_return(615_578) }
 
-    before { expect(war).to receive(:mutual).and_return(false) }
+    before { expect(war).to receive(:mutual).and_return(mutual) }
 
-    before { expect(war).to receive(:open_for_allies).and_return(false) }
+    before { expect(war).to receive(:open_for_allies).and_return(open_for_allies) }
 
     before { expect(war).to receive(:retracted).and_return(retracted) }
 
@@ -46,12 +50,69 @@ describe EveOnline::ESI::Models::War do
 
     its([:war_id]) { should eq(615_578) }
 
-    its([:mutual]) { should eq(false) }
+    its([:mutual]) { should eq(mutual) }
 
-    its([:open_for_allies]) { should eq(false) }
+    its([:open_for_allies]) { should eq(open_for_allies) }
 
     its([:retracted]) { should eq(retracted) }
 
     its([:started]) { should eq(started) }
   end
+
+  describe '#declared' do
+    context 'when declared is present' do
+      let(:declared) { double }
+
+      before { expect(options).to receive(:[]).with('declared').and_return(declared) }
+
+      before do
+        #
+        # subject.parse_datetime_with_timezone(declared)
+        #
+        expect(subject).to receive(:parse_datetime_with_timezone).with(declared)
+      end
+
+      specify { expect { subject.declared }.not_to raise_error }
+    end
+
+    context 'when declared not present' do
+      before { expect(options).to receive(:[]).with('declared').and_return(nil) }
+
+      before { expect(subject).not_to receive(:parse_datetime_with_timezone) }
+
+      specify { expect { subject.declared }.not_to raise_error }
+    end
+  end
+
+
+  # def finished
+  #   finished = options['finished']
+  #
+  #   parse_datetime_with_timezone(finished) if finished
+  # end
+  #
+  # def war_id
+  #   options['id']
+  # end
+  #
+  # def mutual
+  #   options['mutual']
+  # end
+  #
+  # def open_for_allies
+  #   options['open_for_allies']
+  # end
+  #
+  # def retracted
+  #   retracted = options['retracted']
+  #
+  #   parse_datetime_with_timezone(retracted) if retracted
+  # end
+  #
+  # def started
+  #   started = options['started']
+  #
+  #   parse_datetime_with_timezone(started) if started
+  # end
+
 end
