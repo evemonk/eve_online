fe2dc1af719d5170fe8501034f69dd2d2db1c201
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-12 02:15:04 +0300

    Update specs

--- spec/features/alliances_spec.rb
+++ spec/features/alliances_spec.rb
@@ -14,15 +14,15 @@ describe "List all alliances" do
 
     specify { expect(subject.scope).to eq(nil) }
 
-    specify { expect(subject.alliance_ids.size).to eq(3028) }
+    specify { expect(subject.alliance_ids.size).to eq(3072) }
 
-    specify { expect(subject.alliance_ids.first).to eq(1_354_830_081) }
+    specify { expect(subject.alliance_ids.first).to eq(99_000_006) }
 
-    specify { expect(subject.etag).to eq("97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a") }
+    specify { expect(subject.etag).to eq("19defb9a9875a079383cda4410475eea6616840593dead413fc0dbfb") }
 
     specify { expect(subject.error_limit_remain).to eq(100) }
 
-    specify { expect(subject.error_limit_reset).to eq(35) }
+    specify { expect(subject.error_limit_reset).to eq(38) }
   end
 
   context "when etag is set" do
@@ -30,14 +30,14 @@ describe "List all alliances" do
 
     after { VCR.eject_cassette }
 
-    subject { EveOnline::ESI::Alliances.new(etag: "97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a") }
+    subject { EveOnline::ESI::Alliances.new(etag: "19defb9a9875a079383cda4410475eea6616840593dead413fc0dbfb") }
 
     specify { expect(subject.not_modified?).to eq(true) }
 
-    specify { expect(subject.etag).to eq("97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a") }
+    specify { expect(subject.etag).to eq("19defb9a9875a079383cda4410475eea6616840593dead413fc0dbfb") }
 
     specify { expect(subject.error_limit_remain).to eq(100) }
 
-    specify { expect(subject.error_limit_reset).to eq(14) }
+    specify { expect(subject.error_limit_reset).to eq(37) }
   end
 end
