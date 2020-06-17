22a57d66a55a6f4964cbde7e8a4f3427ee4b9d1c
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-18 01:05:50 +0300

    Add feature spec

---
+++ spec/features/public_contracts_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Get public contracts" do
+  context "when etag not set" do
+    before { VCR.insert_cassette "esi/public_contracts" }
+
+    after { VCR.eject_cassette }
+
+    let(:options) { {region_id: 10_000_043, page: 1} }
+
+    subject { EveOnline::ESI::PublicContracts.new(options) }
+
+    specify { expect(subject.scope).to eq(nil) }
+
+    specify { expect(subject.not_modified?).to eq(false) }
+
+    specify { expect(subject.page).to eq(1) }
+
+    specify { expect(subject.total_pages).to eq(4) }
+
+    specify { expect(subject.contracts.size).to eq(1000) }
+
+    specify do
+      expect(subject.contracts.first.as_json).to eq(buyout: nil,
+                                                    collateral: 0.0,
+                                                    contract_id: 157_834_735,
+                                                    date_expired: "2020-06-17T21:36:41Z",
+                                                    date_issued: "2020-05-20T21:36:41Z",
+                                                    days_to_complete: 0,
+                                                    end_location_id: 60_008_494,
+                                                    for_corporation: true,
+                                                    issuer_corporation_id: 897_372_069,
+                                                    issuer_id: 1_314_102_096,
+                                                    price: 1_250_000_000.0,
+                                                    reward: 0.0,
+                                                    start_location_id: 60_008_494,
+                                                    title: "Apostle Me10/Te16 Complete Bpc 10 Pack",
+                                                    type: "item_exchange",
+                                                    volume: 100.0)
+    end
+
+    specify { expect(subject.etag).to eq("3f7fdf9a69e35bb35d619b6cb5043a2766d11ca6306a8b5af369ce6e") }
+
+    specify { expect(subject.error_limit_remain).to eq(100) }
+
+    specify { expect(subject.error_limit_reset).to eq(7) }
+  end
+
+  context "when etag is set" do
+    before { VCR.insert_cassette "esi/public_contracts_etag" }
+
+    after { VCR.eject_cassette }
+
+    let(:options) do
+      {
+        region_id: 10_000_043,
+        page: 1,
+        etag: "3f7fdf9a69e35bb35d619b6cb5043a2766d11ca6306a8b5af369ce6e"
+      }
+    end
+
+    subject { EveOnline::ESI::PublicContracts.new(options) }
+
+    specify { expect(subject.not_modified?).to eq(true) }
+
+    specify { expect(subject.etag).to eq("3f7fdf9a69e35bb35d619b6cb5043a2766d11ca6306a8b5af369ce6e") }
+
+    specify { expect(subject.error_limit_remain).to eq(100) }
+
+    specify { expect(subject.error_limit_reset).to eq(42) }
+  end
+end
