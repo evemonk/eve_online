fb37f4123406efe73dc635d20e73256f84dfcf3c
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-18 22:51:47 +0300

    More specs

--- spec/eve_online/esi/public_contracts_spec.rb
+++ spec/eve_online/esi/public_contracts_spec.rb
@@ -47,7 +47,47 @@ describe EveOnline::ESI::PublicContracts do
     end
 
     context "when @contracts not set" do
-      # TODO: write
+      let(:contract) { instance_double(EveOnline::ESI::Models::PublicContract) }
+
+      let(:response) do
+        [
+          {
+            collateral: 0.0,
+            contract_id: 157_834_735,
+            date_expired: "2020-06-17T21:36:41Z",
+            date_issued: "2020-05-20T21:36:41Z",
+            days_to_complete: 0,
+            end_location_id: 60008494,
+            for_corporation: true,
+            issuer_corporation_id: 897372069,
+            issuer_id: 1314102096,
+            price: 1250000000.0,
+            reward: 0.0,
+            start_location_id: 60008494,
+            title: "ApostleMe10\/Te16 Complete Bpc 10 Pack",
+            type: "item_exchange",
+            volume: 100.0
+          }
+        ]
+      end
+
+      before do
+        #
+        # subject.response # => [{"collateral"=>0.0, "contract_id"=>157834735, "date_expired"=>"2020-06-17T21:36:41Z", "date_issued"=>"2020-05-20T21:36:41Z", "days_to_complete"=>0, "end_location_id"=>60008494, "for_corporation"=>true, "issuer_corporation_id"=>897372069, "issuer_id"=>1314102096, "price"=>1250000000.0, "reward"=>0.0, "start_location_id"=>60008494, "title"=>"ApostleMe10\/Te16 Complete Bpc 10 Pack", "type"=>"item_exchange", "volume"=>100.0}]
+        #
+        expect(subject).to receive(:response).and_return(response)
+      end
+
+      before do
+        #
+        # EveOnline::ESI::Models::PublicContract.new(response.first) # => contract
+        #
+        expect(EveOnline::ESI::Models::PublicContract).to receive(:new).with(response.first).and_return(contract)
+      end
+
+      specify { expect(subject.contracts).to eq([contract]) }
+
+      specify { expect { subject.contracts }.to change { subject.instance_variable_get(:@contracts) }.from(nil).to([contract]) }
     end
   end
 
