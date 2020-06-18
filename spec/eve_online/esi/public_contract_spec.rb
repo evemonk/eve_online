57c8ef9edfad4cc2d07e6d9b7519403faed4f982
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-19 01:51:58 +0300

    More specs

---
+++ spec/eve_online/esi/public_contract_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe EveOnline::ESI::PublicContract do
+  let(:options) { {contract_id: 157_872_948} }
+
+  subject { described_class.new(options) }
+
+  specify { expect(subject).to be_a(EveOnline::ESI::Base) }
+
+  specify { expect(described_class::API_PATH).to eq("/v1/contracts/public/items/%<contract_id>s/") }
+
+  describe "#initialize" do
+    context "without options" do
+      its(:token) { should eq(nil) }
+
+      its(:parser) { should eq(JSON) }
+
+      its(:_read_timeout) { should eq(60) }
+
+      its(:_open_timeout) { should eq(60) }
+
+      if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.6.0")
+        its(:_write_timeout) { should eq(60) }
+      end
+
+      its(:contract_id) { should eq(157_872_948) }
+
+      its(:page) { should eq(1) }
+    end
+
+    context "with options" do
+      let(:options) { {contract_id: 157_872_948, page: 10} }
+
+      its(:page) { should eq(10) }
+    end
+  end
+
+  describe "#items" do
+    # TODO: write
+  end
+
+  describe "#scope" do
+    specify { expect(subject.scope).to eq(nil) }
+  end
+
+  describe "#additional_query_params" do
+    specify { expect(subject.additional_query_params).to eq([:page]) }
+  end
+
+  describe "#path" do
+    specify do
+      expect(subject.path).to eq("/v1/contracts/public/items/157872948/")
+    end
+  end
+
+  describe "#query" do
+    specify do
+      expect(subject.query).to eq(page: 1)
+    end
+  end
+
+  describe "#url" do
+    specify do
+      expect(subject.url).to eq("https://esi.evetech.net/v1/contracts/public/items/157872948/?page=1")
+    end
+  end
+end
