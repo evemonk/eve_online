69effd10793270162b1d98c5f5b148cac3ebf586
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-25 03:15:23 +0300

    Update spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -4,6 +4,10 @@ require "spec_helper"
 
 describe "Search for a resource" do
   context "when etag not set" do
+    before { VCR.insert_cassette "esi/search" }
+
+    after { VCR.eject_cassette }
+
     let(:options) do
       {
         search: "Jita",
@@ -12,28 +16,37 @@ describe "Search for a resource" do
       }
     end
 
-    before { VCR.insert_cassette "esi/search" }
-
-    after { VCR.eject_cassette }
-
     subject { EveOnline::ESI::Search.new(options) }
 
     specify { expect(subject.scope).to eq(nil) }
 
     specify { expect(subject.not_modified?).to eq(false) }
 
-    specify { expect(subject.solar_system.size).to eq(1) }
+    specify do
+      expect(subject.as_json).to eq(agent_ids: [],
+                                    alliance_ids: [],
+                                    character_ids: [],
+                                    constellation_ids: [],
+                                    corporation_ids: [],
+                                    faction_ids: [],
+                                    inventory_type_ids: [],
+                                    region_ids: [],
+                                    solar_system_ids: [30_000_142],
+                                    station_ids: [])
+    end
 
-    specify { expect(subject.as_json).to eq(solar_system: [30000142]) }
+    specify { expect(subject.etag).to eq("fe8f7a2c7e0b10825f52961747c22b7522b499af4e0e51b6cb28bee3") }
 
     specify { expect(subject.error_limit_remain).to eq(100) }
 
     specify { expect(subject.error_limit_reset).to eq(52) }
-
-    specify { expect(subject.etag).to eq("fe8f7a2c7e0b10825f52961747c22b7522b499af4e0e51b6cb28bee3") }
   end
 
   context "when etag is set" do
+    before { VCR.insert_cassette "esi/search_with_etag" }
+
+    after { VCR.eject_cassette }
+
     let(:options) do
       {
         search: "Jita",
@@ -43,13 +56,7 @@ describe "Search for a resource" do
       }
     end
 
-    before { VCR.insert_cassette "esi/search_with_etag" }
-
-    after { VCR.eject_cassette }
-
     subject { EveOnline::ESI::Search.new(options) }
-
-    specify { expect(subject.scope).to eq(nil) }
 
     specify { expect(subject.not_modified?).to eq(true) }
 
