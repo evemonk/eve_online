f846bebc6fd01f2eca3375630314571e820c2756
Igor Zubkov <igor.zubkov@gmail.com>
2021-09-06 00:20:03 +0300

    Update

--- lib/eve_online/esi/character_send_email.rb
+++ lib/eve_online/esi/character_send_email.rb
@@ -3,10 +3,26 @@
 module EveOnline
   module ESI
     class CharacterSendEmail < Base
-      API_PATH = "/v1/characters/%<character_id>/mail/"
+      API_PATH = "/v1/characters/%<character_id>s/mail/"
+
+      attr_reader :character_id, :subject, :body, :recipients, :approved_cost
+
+      def initialize(options)
+        super
+
+        @character_id = options.fetch(:character_id)
+        @subject = options.fetch(:subject)
+        @body = options.fetch(:body)
+        @recipients = options.fetch(:recipients)
+        @approved_cost = options.fetch(:approved_cost, 0)
+      end
 
       def scope
         "esi-mail.send_mail.v1"
+      end
+
+      def path
+        format(API_PATH, character_id: character_id)
       end
     end
   end
--- spec/eve_online/esi/character_send_email_spec.rb
+++ spec/eve_online/esi/character_send_email_spec.rb
@@ -3,25 +3,88 @@
 require "spec_helper"
 
 describe EveOnline::ESI::CharacterSendEmail do
-  let(:options) { {token: "token123"} }
+  let(:options) do
+    {
+      token: "token123",
+      character_id: 12_345_678,
+      subject: "Test subject",
+      body: "Test body",
+      recipients: []
+    }
+  end
 
   subject { described_class.new(options) }
 
   specify { expect(subject).to be_a(EveOnline::ESI::Base) }
 
-  specify { expect(described_class::API_PATH).to eq("/v1/characters/%<character_id>/mail/") }
+  specify { expect(described_class::API_PATH).to eq("/v1/characters/%<character_id>s/mail/") }
 
   describe "#initialize" do
-    its(:token) { should eq("token123") }
+    context "with default approved_cost" do
+      let(:options) do
+        {
+          token: "token123",
+          character_id: 12_345_678,
+          subject: "Test subject",
+          body: "Test body",
+          recipients: []
+        }
+      end
 
-    its(:_read_timeout) { should eq(60) }
+      its(:token) { should eq("token123") }
 
-    its(:_open_timeout) { should eq(60) }
+      its(:_read_timeout) { should eq(60) }
 
-    its(:_write_timeout) { should eq(60) }
+      its(:_open_timeout) { should eq(60) }
+
+      its(:_write_timeout) { should eq(60) }
+
+      its(:character_id) { should eq(12_345_678) }
+
+      its(:subject) { should eq("Test subject") }
+
+      its(:body) { should eq("Test body") }
+
+      its(:recipients) { should eq([]) }
+
+      its(:approved_cost) { should eq(0) }
+    end
+
+    context "with approved_cost" do
+      let(:options) do
+        {
+          token: "token123",
+          character_id: 12_345_678,
+          subject: "Test subject",
+          body: "Test body",
+          recipients: [],
+          approved_cost: 100
+        }
+      end
+
+      its(:approved_cost) { should eq(100) }
+    end
   end
 
   describe "#scope" do
     specify { expect(subject.scope).to eq("esi-mail.send_mail.v1") }
+  end
+
+  describe "#path" do
+    specify do
+      expect(subject.path).to eq("/v1/characters/12345678/mail/")
+    end
+  end
+
+  # describe "#query" do
+  #   specify do
+  #     expect(subject.query).to eq({})
+  #   end
+  # end
+
+  describe "#url" do
+    specify do
+      expect(subject.url).to eq("https://esi.evetech.net/v1/characters/12345678/mail/")
+    end
   end
 end
