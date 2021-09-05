beb2425552f4fc839b6c0e99bba9f8c1cd4ed00f
Igor Zubkov <igor.zubkov@gmail.com>
2021-09-06 00:10:16 +0300

    EveOnline::ESI::CharacterSendEmail#API_PATH and #scope

--- lib/eve_online/esi/character_send_email.rb
+++ lib/eve_online/esi/character_send_email.rb
@@ -3,6 +3,11 @@
 module EveOnline
   module ESI
     class CharacterSendEmail < Base
+      API_PATH = "/v1/characters/%<character_id>/mail/"
+
+      def scope
+        "esi-mail.send_mail.v1"
+      end
     end
   end
 end
--- spec/eve_online/esi/character_send_email_spec.rb
+++ spec/eve_online/esi/character_send_email_spec.rb
@@ -3,5 +3,25 @@
 require "spec_helper"
 
 describe EveOnline::ESI::CharacterSendEmail do
+  let(:options) { {token: "token123"} }
+
+  subject { described_class.new(options) }
+
   specify { expect(subject).to be_a(EveOnline::ESI::Base) }
+
+  specify { expect(described_class::API_PATH).to eq("/v1/characters/%<character_id>/mail/") }
+
+  describe "#initialize" do
+    its(:token) { should eq("token123") }
+
+    its(:_read_timeout) { should eq(60) }
+
+    its(:_open_timeout) { should eq(60) }
+
+    its(:_write_timeout) { should eq(60) }
+  end
+
+  describe "#scope" do
+    specify { expect(subject.scope).to eq("esi-mail.send_mail.v1") }
+  end
 end
