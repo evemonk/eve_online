0dfdda172de037d3b671d033cbd445bf578c12eb
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-13 22:36:56 +0100

    Cleanup

--- spec/features/war_killmails_spec.rb
+++ spec/features/war_killmails_spec.rb
@@ -3,57 +3,28 @@
 require "spec_helper"
 
 describe "List kills for a war" do
-  context "when etag not set" do
-    let(:options) { {war_id: 615_578} }
+  let(:options) { {war_id: 615_578} }
 
-    before { VCR.insert_cassette "esi/wars/615578/killmails" }
+  before { VCR.insert_cassette "esi/wars/615578/killmails" }
 
-    after { VCR.eject_cassette }
+  after { VCR.eject_cassette }
 
-    subject { EveOnline::ESI::WarKillmails.new(options) }
+  subject { EveOnline::ESI::WarKillmails.new(options) }
 
-    specify { expect(subject.scope).to eq(nil) }
+  specify { expect(subject.scope).to eq(nil) }
 
-    specify { expect(subject.not_modified?).to eq(false) }
+  specify { expect(subject.page).to eq(1) }
 
-    specify { expect(subject.page).to eq(1) }
+  specify { expect(subject.total_pages).to eq(1) }
 
-    specify { expect(subject.total_pages).to eq(1) }
+  specify { expect(subject.killmails.size).to eq(9) }
 
-    specify { expect(subject.killmails.size).to eq(9) }
-
-    specify do
-      expect(subject.killmails.first.as_json).to eq(killmail_hash: "07f7ef1d7f6090e78d8e85b4a98e680f67b5e9d5",
-        killmail_id: 72_410_059)
-    end
-
-    specify { expect(subject.etag).to eq("12ee36a10ee0dbfb7f6691e1b27c8400fdca645f15e70ebc0a94b7ae") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(50) }
+  specify do
+    expect(subject.killmails.first.as_json).to eq(killmail_hash: "07f7ef1d7f6090e78d8e85b4a98e680f67b5e9d5",
+      killmail_id: 72_410_059)
   end
 
-  context "when etag is set" do
-    let(:options) do
-      {
-        war_id: 615_578,
-        etag: "12ee36a10ee0dbfb7f6691e1b27c8400fdca645f15e70ebc0a94b7ae"
-      }
-    end
+  specify { expect(subject.error_limit_remain).to eq(100) }
 
-    before { VCR.insert_cassette "esi/wars/615578/killmails_with_etag" }
-
-    after { VCR.eject_cassette }
-
-    subject { EveOnline::ESI::WarKillmails.new(options) }
-
-    specify { expect(subject.not_modified?).to eq(true) }
-
-    specify { expect(subject.etag).to eq("12ee36a10ee0dbfb7f6691e1b27c8400fdca645f15e70ebc0a94b7ae") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(2) }
-  end
+  specify { expect(subject.error_limit_reset).to eq(50) }
 end
