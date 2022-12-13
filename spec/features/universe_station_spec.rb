6cc1ef9e0dc4cf5dd61a5647c1c006c8a2cd01d8
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-13 22:47:07 +0100

    Cleanup

--- spec/features/universe_station_spec.rb
+++ spec/features/universe_station_spec.rb
@@ -3,81 +3,52 @@
 require "spec_helper"
 
 describe "Get station information" do
-  context "when etag not set" do
-    before { VCR.insert_cassette "esi/universe/stations/60012526" }
+  before { VCR.insert_cassette "esi/universe/stations/60012526" }
 
-    after { VCR.eject_cassette }
+  after { VCR.eject_cassette }
 
-    let(:options) { {id: 60_012_526} }
+  let(:options) { {id: 60_012_526} }
 
-    subject { EveOnline::ESI::UniverseStation.new(options) }
+  subject { EveOnline::ESI::UniverseStation.new(options) }
 
-    specify { expect(subject.scope).to eq(nil) }
+  specify { expect(subject.scope).to eq(nil) }
 
-    specify { expect(subject.not_modified?).to eq(false) }
-
-    specify do
-      expect(subject.as_json).to eq(max_dockable_ship_volume: 50_000_000.0,
-        name: "Tanoo V - Moon 1 - Ammatar Consulate Bureau",
-        office_rental_cost: 329_553.0,
-        owner: 1_000_126,
-        race_id: 2,
-        reprocessing_efficiency: 0.5,
-        reprocessing_stations_take: 0.05,
-        services: ["bounty-missions",
-          "courier-missions",
-          "interbus",
-          "reprocessing-plant",
-          "market",
-          "stock-exchange",
-          "cloning",
-          "repair-facilities",
-          "fitting",
-          "news",
-          "insurance",
-          "docking",
-          "office-rental",
-          "loyalty-point-store",
-          "navy-offices",
-          "security-offices"],
-        station_id: 60_012_526,
-        system_id: 30_000_001,
-        type_id: 2502)
-    end
-
-    specify do
-      expect(subject.position.as_json).to eq(x: -1_106_145_239_040.0,
-        y: -145_460_060_160.0,
-        z: 182_618_726_400.0)
-    end
-
-    specify { expect(subject.etag).to eq("e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(36) }
+  specify do
+    expect(subject.as_json).to eq(max_dockable_ship_volume: 50_000_000.0,
+      name: "Tanoo V - Moon 1 - Ammatar Consulate Bureau",
+      office_rental_cost: 329_553.0,
+      owner: 1_000_126,
+      race_id: 2,
+      reprocessing_efficiency: 0.5,
+      reprocessing_stations_take: 0.05,
+      services: ["bounty-missions",
+        "courier-missions",
+        "interbus",
+        "reprocessing-plant",
+        "market",
+        "stock-exchange",
+        "cloning",
+        "repair-facilities",
+        "fitting",
+        "news",
+        "insurance",
+        "docking",
+        "office-rental",
+        "loyalty-point-store",
+        "navy-offices",
+        "security-offices"],
+      station_id: 60_012_526,
+      system_id: 30_000_001,
+      type_id: 2502)
   end
 
-  context "when etag is set" do
-    before { VCR.insert_cassette "esi/universe/stations/60012526_with_etag" }
-
-    after { VCR.eject_cassette }
-
-    let(:options) do
-      {
-        id: 60_012_526,
-        etag: "e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b"
-      }
-    end
-
-    subject { EveOnline::ESI::UniverseStation.new(options) }
-
-    specify { expect(subject.not_modified?).to eq(true) }
-
-    specify { expect(subject.etag).to eq("e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(17) }
+  specify do
+    expect(subject.position.as_json).to eq(x: -1_106_145_239_040.0,
+      y: -145_460_060_160.0,
+      z: 182_618_726_400.0)
   end
+
+  specify { expect(subject.error_limit_remain).to eq(100) }
+
+  specify { expect(subject.error_limit_reset).to eq(36) }
 end
--- spec/features/universe_structures_spec.rb
+++ spec/features/universe_structures_spec.rb
@@ -4,87 +4,36 @@ require "spec_helper"
 
 describe "List all public structures" do
   context "without filter" do
-    context "when etag not set" do
-      before { VCR.insert_cassette "esi/universe/structures" }
+    before { VCR.insert_cassette "esi/universe/structures" }
 
-      after { VCR.eject_cassette }
+    after { VCR.eject_cassette }
 
-      subject { EveOnline::ESI::UniverseStructures.new }
+    subject { EveOnline::ESI::UniverseStructures.new }
 
-      specify { expect(subject.scope).to eq(nil) }
+    specify { expect(subject.scope).to eq(nil) }
 
-      specify { expect(subject.not_modified?).to eq(false) }
+    specify { expect(subject.structure_ids.size).to eq(3654) }
 
-      specify { expect(subject.structure_ids.size).to eq(3654) }
+    specify { expect(subject.structure_ids.first).to eq(1_027_528_548_355) }
 
-      specify { expect(subject.structure_ids.first).to eq(1_027_528_548_355) }
+    specify { expect(subject.error_limit_remain).to eq(100) }
 
-      specify { expect(subject.etag).to eq("1b64f9a8268bb3ba350acc7d7116bfd75dd9f88defb1bbc61771aec7") }
-
-      specify { expect(subject.error_limit_remain).to eq(100) }
-
-      specify { expect(subject.error_limit_reset).to eq(35) }
-    end
-
-    context "when etag is set" do
-      before { VCR.insert_cassette "esi/universe/structures_with_etag" }
-
-      after { VCR.eject_cassette }
-
-      subject { EveOnline::ESI::UniverseStructures.new(etag: "1b64f9a8268bb3ba350acc7d7116bfd75dd9f88defb1bbc61771aec7") }
-
-      specify { expect(subject.not_modified?).to eq(true) }
-
-      specify { expect(subject.etag).to eq("1b64f9a8268bb3ba350acc7d7116bfd75dd9f88defb1bbc61771aec7") }
-
-      specify { expect(subject.error_limit_remain).to eq(100) }
-
-      specify { expect(subject.error_limit_reset).to eq(18) }
-    end
+    specify { expect(subject.error_limit_reset).to eq(35) }
   end
 
   context "with filter" do
-    context "when etag not set" do
-      before { VCR.insert_cassette "esi/universe/structures_with_filter" }
+    before { VCR.insert_cassette "esi/universe/structures_with_filter" }
 
-      after { VCR.eject_cassette }
+    after { VCR.eject_cassette }
 
-      subject { EveOnline::ESI::UniverseStructures.new(filter: "market") }
+    subject { EveOnline::ESI::UniverseStructures.new(filter: "market") }
 
-      specify { expect(subject.not_modified?).to eq(false) }
+    specify { expect(subject.structure_ids.size).to eq(108) }
 
-      specify { expect(subject.structure_ids.size).to eq(108) }
+    specify { expect(subject.structure_ids.first).to eq(1_030_490_622_468) }
 
-      specify { expect(subject.structure_ids.first).to eq(1_030_490_622_468) }
+    specify { expect(subject.error_limit_remain).to eq(100) }
 
-      specify { expect(subject.etag).to eq("2a825ab50413f1efe5f558b8d093e1299389b2f2ce3fa191907f7209") }
-
-      specify { expect(subject.error_limit_remain).to eq(100) }
-
-      specify { expect(subject.error_limit_reset).to eq(55) }
-    end
-
-    context "when etag is set" do
-      let(:options) do
-        {
-          filter: "market",
-          etag: "2a825ab50413f1efe5f558b8d093e1299389b2f2ce3fa191907f7209"
-        }
-      end
-
-      before { VCR.insert_cassette "esi/universe/structures_with_filter_and_etag" }
-
-      after { VCR.eject_cassette }
-
-      subject { EveOnline::ESI::UniverseStructures.new(options) }
-
-      specify { expect(subject.not_modified?).to eq(true) }
-
-      specify { expect(subject.etag).to eq("2a825ab50413f1efe5f558b8d093e1299389b2f2ce3fa191907f7209") }
-
-      specify { expect(subject.error_limit_remain).to eq(100) }
-
-      specify { expect(subject.error_limit_reset).to eq(1) }
-    end
+    specify { expect(subject.error_limit_reset).to eq(55) }
   end
 end
