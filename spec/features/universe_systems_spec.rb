3f8eba9b2bac62a9bd7811944b1807db70ce4eb4
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-13 22:40:08 +0100

    Cleanup

--- spec/features/universe_systems_spec.rb
+++ spec/features/universe_systems_spec.rb
@@ -3,41 +3,19 @@
 require "spec_helper"
 
 describe "Get solar systems" do
-  context "when etag not set" do
-    before { VCR.insert_cassette "esi/universe/systems" }
+  before { VCR.insert_cassette "esi/universe/systems" }
 
-    after { VCR.eject_cassette }
+  after { VCR.eject_cassette }
 
-    subject { EveOnline::ESI::UniverseSystems.new }
+  subject { EveOnline::ESI::UniverseSystems.new }
 
-    specify { expect(subject.scope).to eq(nil) }
+  specify { expect(subject.scope).to eq(nil) }
 
-    specify { expect(subject.not_modified?).to eq(false) }
+  specify { expect(subject.universe_system_ids.size).to eq(8285) }
 
-    specify { expect(subject.universe_system_ids.size).to eq(8285) }
+  specify { expect(subject.universe_system_ids.first).to eq(30_000_001) }
 
-    specify { expect(subject.universe_system_ids.first).to eq(30_000_001) }
+  specify { expect(subject.error_limit_remain).to eq(100) }
 
-    specify { expect(subject.etag).to eq("e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(1) }
-  end
-
-  context "when etag is set" do
-    before { VCR.insert_cassette "esi/universe/systems_with_etag" }
-
-    after { VCR.eject_cassette }
-
-    subject { EveOnline::ESI::UniverseSystems.new(etag: "e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
-
-    specify { expect(subject.not_modified?).to eq(true) }
-
-    specify { expect(subject.etag).to eq("e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(55) }
-  end
+  specify { expect(subject.error_limit_reset).to eq(1) }
 end
--- spec/features/universe_types_spec.rb
+++ spec/features/universe_types_spec.rb
@@ -3,45 +3,23 @@
 require "spec_helper"
 
 describe "Get types" do
-  context "when etag not set" do
-    before { VCR.insert_cassette "esi/universe/types" }
+  before { VCR.insert_cassette "esi/universe/types" }
 
-    after { VCR.eject_cassette }
+  after { VCR.eject_cassette }
 
-    subject { EveOnline::ESI::UniverseTypes.new }
+  subject { EveOnline::ESI::UniverseTypes.new }
 
-    specify { expect(subject.scope).to eq(nil) }
+  specify { expect(subject.scope).to eq(nil) }
 
-    specify { expect(subject.not_modified?).to eq(false) }
+  specify { expect(subject.page).to eq(1) }
 
-    specify { expect(subject.page).to eq(1) }
+  specify { expect(subject.total_pages).to eq(36) }
 
-    specify { expect(subject.total_pages).to eq(36) }
+  specify { expect(subject.universe_type_ids.size).to eq(1000) }
 
-    specify { expect(subject.universe_type_ids.size).to eq(1000) }
+  specify { expect(subject.universe_type_ids.first).to eq(0) }
 
-    specify { expect(subject.universe_type_ids.first).to eq(0) }
+  specify { expect(subject.error_limit_remain).to eq(100) }
 
-    specify { expect(subject.etag).to eq("e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(45) }
-  end
-
-  context "when etag is set" do
-    before { VCR.insert_cassette "esi/universe/types_with_etag" }
-
-    after { VCR.eject_cassette }
-
-    subject { EveOnline::ESI::UniverseTypes.new(etag: "e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
-
-    specify { expect(subject.not_modified?).to eq(true) }
-
-    specify { expect(subject.etag).to eq("e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(2) }
-  end
+  specify { expect(subject.error_limit_reset).to eq(45) }
 end
