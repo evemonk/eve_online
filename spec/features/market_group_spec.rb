1c775ead08ad7ba6655e2a7231c03df0ee4ff80b
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-13 23:27:45 +0100

    Cleanup

--- spec/features/market_group_spec.rb
+++ spec/features/market_group_spec.rb
@@ -3,57 +3,28 @@
 require "spec_helper"
 
 describe "Get item group information" do
-  context "when etag not set" do
-    before { VCR.insert_cassette "esi/market/groups/618" }
+  before { VCR.insert_cassette "esi/market/groups/618" }
 
-    after { VCR.eject_cassette }
+  after { VCR.eject_cassette }
 
-    let(:options) { {id: 618} }
+  let(:options) { {id: 618} }
 
-    subject { EveOnline::ESI::MarketGroup.new(options) }
+  subject { EveOnline::ESI::MarketGroup.new(options) }
 
-    specify { expect(subject.not_modified?).to eq(false) }
+  specify { expect(subject.scope).to eq(nil) }
 
-    specify { expect(subject.scope).to eq(nil) }
-
-    specify do
-      expect(subject.as_json).to eq(description: "Implants intended for Subcervical Processing Slot 1.",
-        market_group_id: 618,
-        name: "Implant Slot 01",
-        parent_group_id: 532)
-    end
-
-    specify { expect(subject.type_ids.size).to eq(48) }
-
-    specify { expect(subject.type_ids.first).to eq(28_802) }
-
-    specify { expect(subject.etag).to eq("bf7832bfc20f5f9fdeddc9cb0360b941067310e7e3a2a80315b45a43") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(16) }
+  specify do
+    expect(subject.as_json).to eq(description: "Implants intended for Subcervical Processing Slot 1.",
+      market_group_id: 618,
+      name: "Implant Slot 01",
+      parent_group_id: 532)
   end
 
-  context "when etag is set" do
-    before { VCR.insert_cassette "esi/market/groups/618_with_etag" }
+  specify { expect(subject.type_ids.size).to eq(48) }
 
-    after { VCR.eject_cassette }
+  specify { expect(subject.type_ids.first).to eq(28_802) }
 
-    let(:options) do
-      {
-        id: 618,
-        etag: "bf7832bfc20f5f9fdeddc9cb0360b941067310e7e3a2a80315b45a43"
-      }
-    end
+  specify { expect(subject.error_limit_remain).to eq(100) }
 
-    subject { EveOnline::ESI::MarketGroup.new(options) }
-
-    specify { expect(subject.not_modified?).to eq(true) }
-
-    specify { expect(subject.etag).to eq("bf7832bfc20f5f9fdeddc9cb0360b941067310e7e3a2a80315b45a43") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(32) }
-  end
+  specify { expect(subject.error_limit_reset).to eq(16) }
 end
--- spec/features/server_status_spec.rb
+++ spec/features/server_status_spec.rb
@@ -3,44 +3,22 @@
 require "spec_helper"
 
 describe "Retrieve the uptime and player counts" do
-  context "when etag not set" do
-    before { VCR.insert_cassette "esi/server_status" }
+  before { VCR.insert_cassette "esi/server_status" }
 
-    after { VCR.eject_cassette }
+  after { VCR.eject_cassette }
 
-    subject { EveOnline::ESI::ServerStatus.new }
+  subject { EveOnline::ESI::ServerStatus.new }
 
-    specify { expect(subject.scope).to eq(nil) }
+  specify { expect(subject.scope).to eq(nil) }
 
-    specify { expect(subject.not_modified?).to eq(false) }
-
-    specify do
-      expect(subject.as_json).to eq(players: 20_450,
-        server_version: "1446360",
-        start_time: "2019-01-17T11:05:44Z",
-        vip: nil)
-    end
-
-    specify { expect(subject.etag).to eq("c2f41ae833fceb5533627552f494d071e34d4e4a2183589a6b9c85e1") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(2) }
+  specify do
+    expect(subject.as_json).to eq(players: 20_450,
+      server_version: "1446360",
+      start_time: "2019-01-17T11:05:44Z",
+      vip: nil)
   end
 
-  context "when etag is set" do
-    before { VCR.insert_cassette "esi/server_status_with_etag" }
+  specify { expect(subject.error_limit_remain).to eq(100) }
 
-    after { VCR.eject_cassette }
-
-    subject { EveOnline::ESI::ServerStatus.new(etag: "c2f41ae833fceb5533627552f494d071e34d4e4a2183589a6b9c85e1") }
-
-    specify { expect(subject.not_modified?).to eq(true) }
-
-    specify { expect(subject.etag).to eq("c2f41ae833fceb5533627552f494d071e34d4e4a2183589a6b9c85e1") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(2) }
-  end
+  specify { expect(subject.error_limit_reset).to eq(2) }
 end
