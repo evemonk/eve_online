ff2f737d1d8c9ae72a5ec5cfdcb68ae28e61c449
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-14 13:56:25 +0100

    Cleanup

--- spec/features/character_spec.rb
+++ spec/features/character_spec.rb
@@ -3,60 +3,31 @@
 require "spec_helper"
 
 describe "Get character's public information" do
-  context "when etag not set" do
-    let(:options) { {character_id: 1_337_512_245} }
+  let(:options) { {character_id: 1_337_512_245} }
 
-    before { VCR.insert_cassette "esi/characters/1337512245" }
+  before { VCR.insert_cassette "esi/characters/1337512245" }
 
-    after { VCR.eject_cassette }
+  after { VCR.eject_cassette }
 
-    subject { EveOnline::ESI::Character.new(options) }
+  subject { EveOnline::ESI::Character.new(options) }
 
-    specify { expect(subject.scope).to eq(nil) }
+  specify { expect(subject.scope).to eq(nil) }
 
-    specify { expect(subject.not_modified?).to eq(false) }
-
-    specify do
-      expect(subject.as_json).to eq(corporation_id: 98_565_696,
-        birthday: "2010-01-15T15:26:00Z",
-        name: "Johnn Dillinger",
-        gender: "male",
-        race_id: 2,
-        bloodline_id: 4,
-        description: "",
-        alliance_id: 99_008_595,
-        security_status: 3.83155339,
-        faction_id: nil,
-        title: nil)
-    end
-
-    specify { expect(subject.etag).to eq("5e57d726eda270ba0e8542bdfbca874c844e5cdbf4b5599eeca79d1f") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(47) }
+  specify do
+    expect(subject.as_json).to eq(corporation_id: 98_565_696,
+      birthday: "2010-01-15T15:26:00Z",
+      name: "Johnn Dillinger",
+      gender: "male",
+      race_id: 2,
+      bloodline_id: 4,
+      description: "",
+      alliance_id: 99_008_595,
+      security_status: 3.83155339,
+      faction_id: nil,
+      title: nil)
   end
 
-  context "when etag is set" do
-    let(:options) do
-      {
-        character_id: 1_337_512_245,
-        etag: "5e57d726eda270ba0e8542bdfbca874c844e5cdbf4b5599eeca79d1f"
-      }
-    end
+  specify { expect(subject.error_limit_remain).to eq(100) }
 
-    before { VCR.insert_cassette "esi/characters/1337512245_with_etag" }
-
-    after { VCR.eject_cassette }
-
-    subject { EveOnline::ESI::Character.new(options) }
-
-    specify { expect(subject.not_modified?).to eq(true) }
-
-    specify { expect(subject.etag).to eq("5e57d726eda270ba0e8542bdfbca874c844e5cdbf4b5599eeca79d1f") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(7) }
-  end
+  specify { expect(subject.error_limit_reset).to eq(47) }
 end
--- spec/features/character_wallet_transactions_spec.rb
+++ spec/features/character_wallet_transactions_spec.rb
@@ -3,124 +3,63 @@
 require "spec_helper"
 
 describe "Get wallet transactions" do
-  context "when etag not set" do
-    context "when from_id not set" do
-      let(:options) do
-        {
-          character_id: 1_337_512_245,
-          token: "token123"
-        }
-      end
-
-      before { VCR.insert_cassette "esi/character_wallet_transactions/1337512245" }
-
-      after { VCR.eject_cassette }
-
-      subject { EveOnline::ESI::CharacterWalletTransactions.new(options) }
-
-      specify { expect(subject.scope).to eq("esi-wallet.read_character_wallet.v1") }
-
-      specify { expect(subject.not_modified?).to eq(false) }
-
-      specify { expect(subject.wallet_transactions.size).to eq(47) }
-
-      specify do
-        expect(subject.wallet_transactions.first.as_json).to eq(client_id: 2116253203,
-          date: "2020-03-08T11:49:16Z",
-          is_buy: true,
-          is_personal: true,
-          journal_ref_id: 17740411474,
-          location_id: 60008494,
-          quantity: 1,
-          transaction_id: 5296927639,
-          type_id: 3538,
-          unit_price: 99887.79)
-      end
-
-      specify { expect(subject.etag).to eq("55d0fbe8b408d6a76dab70c0a709d4e7c5dd58d4e0b7bd785549e268") }
-
-      specify { expect(subject.error_limit_remain).to eq(100) }
-
-      specify { expect(subject.error_limit_reset).to eq(48) }
+  context "when from_id not set" do
+    let(:options) do
+      {
+        character_id: 1_337_512_245,
+        token: "token123"
+      }
     end
 
-    context "when from_id is set" do
-      let(:options) do
-        {
-          character_id: 1_337_512_245,
-          token: "token123",
-          from_id: 5278248796
-        }
-      end
+    before { VCR.insert_cassette "esi/character_wallet_transactions/1337512245" }
 
-      before { VCR.insert_cassette "esi/character_wallet_transactions/1337512245_with_from_id" }
+    after { VCR.eject_cassette }
 
-      after { VCR.eject_cassette }
+    subject { EveOnline::ESI::CharacterWalletTransactions.new(options) }
 
-      subject { EveOnline::ESI::CharacterWalletTransactions.new(options) }
+    specify { expect(subject.scope).to eq("esi-wallet.read_character_wallet.v1") }
 
-      specify { expect(subject.scope).to eq("esi-wallet.read_character_wallet.v1") }
+    specify { expect(subject.wallet_transactions.size).to eq(47) }
 
-      specify { expect(subject.not_modified?).to eq(false) }
-
-      specify { expect(subject.wallet_transactions.size).to eq(1) }
-
-      specify { expect(subject.etag).to eq("b92fae885ba527533c0696e80bc5283600c3cb004e88651f9afea02d") }
-
-      specify { expect(subject.error_limit_remain).to eq(100) }
-
-      specify { expect(subject.error_limit_reset).to eq(7) }
+    specify do
+      expect(subject.wallet_transactions.first.as_json).to eq(client_id: 2116253203,
+        date: "2020-03-08T11:49:16Z",
+        is_buy: true,
+        is_personal: true,
+        journal_ref_id: 17740411474,
+        location_id: 60008494,
+        quantity: 1,
+        transaction_id: 5296927639,
+        type_id: 3538,
+        unit_price: 99887.79)
     end
+
+    specify { expect(subject.error_limit_remain).to eq(100) }
+
+    specify { expect(subject.error_limit_reset).to eq(48) }
   end
 
-  context "when etag is set" do
-    context "when from_id not set" do
-      let(:options) do
-        {
-          character_id: 1_337_512_245,
-          token: "token123",
-          etag: "55d0fbe8b408d6a76dab70c0a709d4e7c5dd58d4e0b7bd785549e268"
-        }
-      end
-
-      before { VCR.insert_cassette "esi/character_wallet_transactions/1337512245_with_etag" }
-
-      after { VCR.eject_cassette }
-
-      subject { EveOnline::ESI::CharacterWalletTransactions.new(options) }
-
-      specify { expect(subject.not_modified?).to eq(true) }
-
-      specify { expect(subject.etag).to eq("55d0fbe8b408d6a76dab70c0a709d4e7c5dd58d4e0b7bd785549e268") }
-
-      specify { expect(subject.error_limit_remain).to eq(100) }
-
-      specify { expect(subject.error_limit_reset).to eq(49) }
+  context "when from_id is set" do
+    let(:options) do
+      {
+        character_id: 1_337_512_245,
+        token: "token123",
+        from_id: 5278248796
+      }
     end
 
-    context "when from_id is set" do
-      let(:options) do
-        {
-          character_id: 1_337_512_245,
-          token: "token123",
-          etag: "b92fae885ba527533c0696e80bc5283600c3cb004e88651f9afea02d",
-          from_id: 5278248796
-        }
-      end
+    before { VCR.insert_cassette "esi/character_wallet_transactions/1337512245_with_from_id" }
 
-      before { VCR.insert_cassette "esi/character_wallet_transactions/1337512245_with_from_id_with_etag" }
+    after { VCR.eject_cassette }
 
-      after { VCR.eject_cassette }
+    subject { EveOnline::ESI::CharacterWalletTransactions.new(options) }
 
-      subject { EveOnline::ESI::CharacterWalletTransactions.new(options) }
+    specify { expect(subject.scope).to eq("esi-wallet.read_character_wallet.v1") }
 
-      specify { expect(subject.not_modified?).to eq(true) }
+    specify { expect(subject.wallet_transactions.size).to eq(1) }
 
-      specify { expect(subject.etag).to eq("b92fae885ba527533c0696e80bc5283600c3cb004e88651f9afea02d") }
+    specify { expect(subject.error_limit_remain).to eq(100) }
 
-      specify { expect(subject.error_limit_remain).to eq(100) }
-
-      specify { expect(subject.error_limit_reset).to eq(28) }
-    end
+    specify { expect(subject.error_limit_reset).to eq(7) }
   end
 end
