0dfc0ca344807df07baf7dec3a6c7b7b9c3097ff
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-14 14:01:25 +0100

    Cleanup

--- spec/features/character_mail_labels_spec.rb
+++ spec/features/character_mail_labels_spec.rb
@@ -3,63 +3,33 @@
 require "spec_helper"
 
 describe "Get mail labels and unread counts" do
-  context "when etag not set" do
-    before { VCR.insert_cassette "esi/mail/labels_and_unread_counts" }
+  before { VCR.insert_cassette "esi/mail/labels_and_unread_counts" }
 
-    after { VCR.eject_cassette }
+  after { VCR.eject_cassette }
 
-    let(:options) do
-      {
-        character_id: 90_729_314,
-        token: "token123"
-      }
-    end
-
-    subject { EveOnline::ESI::CharacterMailLabels.new(options) }
-
-    specify { expect(subject.not_modified?).to eq(false) }
-
-    specify { expect(subject.scope).to eq("esi-mail.read_mail.v1") }
-
-    specify { expect(subject.total_unread_count).to eq(1) }
-
-    specify { expect(subject.labels.size).to eq(4) }
-
-    specify do
-      expect(subject.labels.last.as_json).to eq(color: "#ffffff",
-        label_id: 8,
-        name: "[Alliance]",
-        unread_count: 0)
-    end
-
-    specify { expect(subject.etag).to eq("6aac63dafa81b592eba22fe0d7bf12a2c11bfd5b054aa78d3a3a5cde") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(43) }
+  let(:options) do
+    {
+      character_id: 90_729_314,
+      token: "token123"
+    }
   end
 
-  context "when etag is set" do
-    before { VCR.insert_cassette "esi/mail/labels_and_unread_counts_with_etag" }
+  subject { EveOnline::ESI::CharacterMailLabels.new(options) }
 
-    after { VCR.eject_cassette }
+  specify { expect(subject.scope).to eq("esi-mail.read_mail.v1") }
 
-    let(:options) do
-      {
-        character_id: 90_729_314,
-        token: "token123",
-        etag: "6aac63dafa81b592eba22fe0d7bf12a2c11bfd5b054aa78d3a3a5cde"
-      }
-    end
+  specify { expect(subject.total_unread_count).to eq(1) }
 
-    subject { EveOnline::ESI::CharacterMailLabels.new(options) }
+  specify { expect(subject.labels.size).to eq(4) }
 
-    specify { expect(subject.not_modified?).to eq(true) }
-
-    specify { expect(subject.etag).to eq("6aac63dafa81b592eba22fe0d7bf12a2c11bfd5b054aa78d3a3a5cde") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(37) }
+  specify do
+    expect(subject.labels.last.as_json).to eq(color: "#ffffff",
+      label_id: 8,
+      name: "[Alliance]",
+      unread_count: 0)
   end
+
+  specify { expect(subject.error_limit_remain).to eq(100) }
+
+  specify { expect(subject.error_limit_reset).to eq(43) }
 end
--- spec/features/character_online_spec.rb
+++ spec/features/character_online_spec.rb
@@ -3,59 +3,29 @@
 require "spec_helper"
 
 describe "Get character online" do
-  context "when etag not set" do
-    before { VCR.insert_cassette "esi/location/online" }
+  before { VCR.insert_cassette "esi/location/online" }
 
-    after { VCR.eject_cassette }
+  after { VCR.eject_cassette }
 
-    let(:options) do
-      {
-        character_id: 90_729_314,
-        token: "token123"
-      }
-    end
-
-    subject { EveOnline::ESI::CharacterOnline.new(options) }
-
-    specify { expect(subject.not_modified?).to eq(false) }
-
-    specify { expect(subject.scope).to eq("esi-location.read_online.v1") }
-
-    specify do
-      expect(subject.as_json).to eq(last_login: "2017-01-15 11:39:24",
-        last_logout: "2017-01-15 11:31:22",
-        logins: 370,
-        online: false)
-    end
-
-    specify { expect(subject.etag).to eq("43c82cdefedc4275da30d7731200df96b905dc94b8486d55bedb5fe6") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(49) }
+  let(:options) do
+    {
+      character_id: 90_729_314,
+      token: "token123"
+    }
   end
 
-  context "when etag is set" do
-    before { VCR.insert_cassette "esi/location/online_with_etag" }
+  subject { EveOnline::ESI::CharacterOnline.new(options) }
 
-    after { VCR.eject_cassette }
+  specify { expect(subject.scope).to eq("esi-location.read_online.v1") }
 
-    let(:options) do
-      {
-        character_id: 90_729_314,
-        token: "token123",
-        etag: "43c82cdefedc4275da30d7731200df96b905dc94b8486d55bedb5fe6"
-      }
-    end
-
-    subject { EveOnline::ESI::CharacterOnline.new(options) }
-
-    specify { expect(subject.not_modified?).to eq(true) }
-
-    specify { expect(subject.etag).to eq("43c82cdefedc4275da30d7731200df96b905dc94b8486d55bedb5fe6") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(5) }
+  specify do
+    expect(subject.as_json).to eq(last_login: "2017-01-15 11:39:24",
+      last_logout: "2017-01-15 11:31:22",
+      logins: 370,
+      online: false)
   end
+
+  specify { expect(subject.error_limit_remain).to eq(100) }
+
+  specify { expect(subject.error_limit_reset).to eq(49) }
 end
