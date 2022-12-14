493895287fe9d7e35e8777edcbe5e77cd20704ba
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-14 13:50:04 +0100

    Cleanup

--- spec/features/dogma_attribute_spec.rb
+++ spec/features/dogma_attribute_spec.rb
@@ -3,59 +3,30 @@
 require "spec_helper"
 
 describe "Get attribute information" do
-  context "when etag not set" do
-    before { VCR.insert_cassette "esi/dogma/attributes/2" }
+  before { VCR.insert_cassette "esi/dogma/attributes/2" }
 
-    after { VCR.eject_cassette }
+  after { VCR.eject_cassette }
 
-    let(:options) { {id: 2} }
+  let(:options) { {id: 2} }
 
-    subject { EveOnline::ESI::DogmaAttribute.new(options) }
+  subject { EveOnline::ESI::DogmaAttribute.new(options) }
 
-    specify { expect(subject.scope).to eq(nil) }
+  specify { expect(subject.scope).to eq(nil) }
 
-    specify { expect(subject.not_modified?).to eq(false) }
-
-    specify do
-      expect(subject.as_json).to eq(attribute_id: 2,
-        default_value: 0.0,
-        description: "Boolean to store status of online effect",
-        display_name: "",
-        high_is_good: true,
-        icon_id: nil,
-        name: "isOnline",
-        published: nil,
-        stackable: true,
-        unit_id: nil)
-    end
-
-    specify { expect(subject.etag).to eq("acc1e563574a55f79ebe4a2a99845dd6c28f4e412e11fd084b8485fd") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(19) }
+  specify do
+    expect(subject.as_json).to eq(attribute_id: 2,
+      default_value: 0.0,
+      description: "Boolean to store status of online effect",
+      display_name: "",
+      high_is_good: true,
+      icon_id: nil,
+      name: "isOnline",
+      published: nil,
+      stackable: true,
+      unit_id: nil)
   end
 
-  context "when etag is set" do
-    before { VCR.insert_cassette "esi/dogma/attributes/2_with_etag" }
+  specify { expect(subject.error_limit_remain).to eq(100) }
 
-    after { VCR.eject_cassette }
-
-    let(:options) do
-      {
-        id: 2,
-        etag: "acc1e563574a55f79ebe4a2a99845dd6c28f4e412e11fd084b8485fd"
-      }
-    end
-
-    subject { EveOnline::ESI::DogmaAttribute.new(options) }
-
-    specify { expect(subject.not_modified?).to eq(true) }
-
-    specify { expect(subject.etag).to eq("acc1e563574a55f79ebe4a2a99845dd6c28f4e412e11fd084b8485fd") }
-
-    specify { expect(subject.error_limit_remain).to eq(100) }
-
-    specify { expect(subject.error_limit_reset).to eq(18) }
-  end
+  specify { expect(subject.error_limit_reset).to eq(19) }
 end
