3f398acac869fa7e8704e8db4992f712026640db
Igor Zubkov <igor.zubkov@gmail.com>
2025-01-02 12:14:25 +0100

    Fix tests

--- spec/eve_online/esi/base_spec.rb
+++ spec/eve_online/esi/base_spec.rb
@@ -453,7 +453,7 @@ describe EveOnline::ESI::Base do
     end
   end
 
-  fdescribe "#resource" do
+  describe "#resource" do
     context "when @resource set" do
       let(:resource) { double }
 
@@ -485,21 +485,21 @@ describe EveOnline::ESI::Base do
     end
 
     context "when throw Faraday::ConnectionFailed" do
-      # let(:http_method) { double }
-      #
-      # let(:uri) { double }
-      #
-      # let(:connection) { double }
-      #
-      # before { expect(subject).to receive(:http_method).and_return(http_method) }
-      #
-      # before { expect(subject).to receive(:uri).and_return(uri) }
-      #
-      # before { expect(subject).to receive(:connection).and_return(connection) }
-      #
-      # before { expect(connection).to receive(:public_send).with(http_method, uri).and_raise(Faraday::ConnectionFailed, nil) }
-      #
-      # specify { expect { subject.resource }.to raise_error(EveOnline::Exceptions::Timeout) }
+      let(:http_method) { :get }
+
+      let(:uri) { double }
+
+      let(:connection) { double }
+
+      before { expect(subject).to receive(:http_method).and_return(http_method).twice }
+
+      before { expect(subject).to receive(:uri).and_return(uri) }
+
+      before { expect(subject).to receive(:connection).and_return(connection) }
+
+      before { expect(connection).to receive(:public_send).with(http_method, uri).and_raise(Faraday::ConnectionFailed, nil) }
+
+      specify { expect { subject.resource }.to raise_error(EveOnline::Exceptions::Timeout) }
     end
 
     context "when throw Faraday::TimeoutError" do
