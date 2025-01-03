ab820a08494c6ef7f35414d03ed3199f918d1b37
Igor Zubkov <igor.zubkov@gmail.com>
2025-01-03 09:29:16 +0100

    Update tests

--- spec/features/universe_factions_spec.rb
+++ spec/features/universe_factions_spec.rb
@@ -11,22 +11,22 @@ describe "Get factions" do
 
   specify { expect(subject.scope).to eq(nil) }
 
-  specify { expect(subject.factions.size).to eq(24) }
+  specify { expect(subject.factions.size).to eq(27) }
 
   specify do
-    expect(subject.factions.first.as_json).to eq(corporation_id: 1_000_035,
-      description: "The Caldari State is ruled by several mega-corporations. There is no central government to speak of - all territories within the State are owned and ruled by corporations. Duty and discipline are required traits in Caldari citizens, plus unquestioning loyalty to the corporation they live to serve. The corporations compete aggressively amongst themselves and with companies outside the State, resulting in a highly capitalistic society.",
-      faction_id: 500_001,
+    expect(subject.factions.first.as_json).to eq(corporation_id: 1_000_084,
+      description: "The largest of the five main empires, the Amarr Empire is a sprawling patch-work of feudal-like provinces held together by the might of the emperor. Religion has always played a big part in Amarrian politics and the Amarrians believe they are the rightful masters of the world, souring their relations with their neighbours. Another source of ill-feelings on part of the other empires is the fact that the Amarrians embrace slavery.",
+      faction_id: 500_003,
       is_unique: true,
-      militia_corporation_id: 1_000_180,
-      name: "Caldari State",
+      militia_corporation_id: 1_000_179,
+      name: "Amarr Empire",
       size_factor: 5.0,
-      solar_system_id: 30_000_145,
-      station_count: 1503,
-      station_system_count: 503)
+      solar_system_id: 30_002_187,
+      station_count: 1032,
+      station_system_count: 508)
   end
 
   specify { expect(subject.error_limit_remain).to eq(100) }
 
-  specify { expect(subject.error_limit_reset).to eq(34) }
+  specify { expect(subject.error_limit_reset).to eq(13) }
 end
