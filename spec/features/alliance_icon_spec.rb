f5dae111067221150b47c4ed5fb010270d2154e0
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-03 14:23:41 +0100

    Update

--- spec/features/alliance_icon_spec.rb
+++ spec/features/alliance_icon_spec.rb
@@ -3,22 +3,34 @@
 require "spec_helper"
 
 describe "Get alliance icon" do
-  let(:options) { {alliance_id: 99_008_595} }
-
-  before { VCR.insert_cassette "esi/alliance_icon/99008595" }
+  before { VCR.insert_cassette "esi/alliance_icon/99008595_new" }
 
   after { VCR.eject_cassette }
 
-  subject { EveOnline::ESI::AllianceIcon.new(options) }
+  let(:client) { EveOnline::ESI::Client.new }
 
-  specify { expect(subject.scope).to eq(nil) }
+  subject { client.alliances.icons(id: 99_008_595) }
 
   specify do
     expect(subject.as_json).to eq(icon_medium: "https://images.evetech.net/alliances/99008595/logo?tenant=tranquility&size=128",
       icon_small: "https://images.evetech.net/alliances/99008595/logo?tenant=tranquility&size=64")
   end
 
+  specify { expect(subject.etag).to eq('"d53e06315fe6f15f4dd47da86f16b3cb51977abc22701227d931f03b"') }
+
+  specify { expect(subject.cache_status).to eq("MISS") }
+
+  specify { expect(subject.request_id).to eq("bf2c1384-ddd0-4d07-b53e-ccdfd2cfce4f") }
+
+  specify { expect(subject.ratelimit_group).to eq(nil) }
+
+  specify { expect(subject.ratelimit_limit).to eq(nil) }
+
+  specify { expect(subject.ratelimit_remaining).to eq(nil) }
+
+  specify { expect(subject.ratelimit_used).to eq(nil) }
+
   specify { expect(subject.error_limit_remain).to eq(100) }
 
-  specify { expect(subject.error_limit_reset).to eq(10) }
+  specify { expect(subject.error_limit_reset).to eq(12) }
 end
