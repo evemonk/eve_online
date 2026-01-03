940f3b6979e1eb0327e19097213522af3e3d7112
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-03 19:16:26 +0100

    Update

--- lib/eve_online/esi/resources/assets_resource.rb
+++ lib/eve_online/esi/resources/assets_resource.rb
@@ -4,9 +4,10 @@ module EveOnline
   module ESI
     module Resources
       class AssetsResource < Resource
-        # @param id [Integer] The ID of the character
-        def character(id:)
-          response = get_request("characters/#{id}/assets")
+        # @param id [Integer] The ID of the character.
+        # @param page [Integer] Page ID. Default: `1`.
+        def character(id:, page: 1)
+          response = get_request("characters/#{id}/assets", params: {page: page})
 
           Collection.from_response(response, type: Models::CharacterAsset)
         end
