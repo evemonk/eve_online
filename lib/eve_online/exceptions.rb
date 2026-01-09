154dba819fdc52dea8a2adab2d556a2775c35e5f
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-09 14:54:23 +0100

    Add notes

--- lib/eve_online/exceptions.rb
+++ lib/eve_online/exceptions.rb
@@ -5,8 +5,8 @@ module EveOnline
     # This is base EveOnline exception class. Rescue it if you want to
     # catch any exceptions from EveOnline.
     class Base < RuntimeError; end
-    class BadGateway < Base; end
-    class BadRequest < Base; end
+    class BadGateway < Base; end # 502
+    class BadRequest < Base; end # 400
     class ErrorLimited < Base; end
     class Forbidden < Base; end
     class InternalServerError < Base; end
