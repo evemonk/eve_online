2c9ebd6607f9f801f91c27f102a682083a6342a2
Igor Zubkov <igor.zubkov@gmail.com>
2026-02-14 19:53:46 +0100

    Update

--- lib/eve_online/esi/resources/location_resources.rb
+++ lib/eve_online/esi/resources/location_resources.rb
@@ -6,7 +6,7 @@ module EveOnline
       class LocationResources < Resource
         # @param character_id [Integer] A character ID
         def ship(character_id:)
-
+          response = get_request("characters/#{character_id}/ship")
         end
       end
     end
