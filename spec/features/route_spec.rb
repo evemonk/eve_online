3bd7a1b85608ea9206f7e8e550b3f0a9a0a8f51d
Igor Zubkov <igor.zubkov@gmail.com>
2026-02-14 16:37:01 +0100

    Update

--- spec/features/route_spec.rb
+++ spec/features/route_spec.rb
@@ -53,4 +53,37 @@ describe "Get route between two systems" do
 
     specify { expect(subject.error_limit_reset).to eq(nil) }
   end
+
+  context "when security_penalty set" do
+    before { VCR.insert_cassette "esi/routes/security_penalty" }
+
+    after { VCR.eject_cassette }
+
+    let(:jita_system_id) { 30_000_142 }
+
+    let(:amarr_system_id) { 30_002_187 }
+
+    let(:security_penalty) { 0 }
+
+    let(:jita_to_amarr) do
+      [
+        30_000_142,
+        30_000_138,
+        30_000_132,
+        30_000_134,
+        30_005_196,
+        30_005_192,
+        30_004_083,
+        30_004_081,
+        30_002_197,
+        30_002_193,
+        30_003_491,
+        30_002_187
+      ]
+    end
+
+    subject { client.routes.route(destination_system_id: amarr_system_id, origin_system_id: jita_system_id, security_penalty: security_penalty) }
+
+    specify { expect(subject.route).to eq(jita_to_amarr) }
+  end
 end
