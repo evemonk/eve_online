178be74505bd4a5e58c6a7aeb5fc78487c8405c3
Igor Zubkov <igor.zubkov@gmail.com>
2026-02-20 19:33:34 +0100

    Update

--- spec/features/wars_spec.rb
+++ spec/features/wars_spec.rb
@@ -35,19 +35,17 @@ RSpec.describe "List wars" do
     specify { expect(subject.error_limit_reset).to eq(nil) }
   end
 
-  # context "with max_war_id" do
-  #   before { VCR.insert_cassette "esi/wars/wars_with_max_war_id" }
-  #
-  #   after { VCR.eject_cassette }
-  #
-  #   subject { EveOnline::ESI::Wars.new(max_war_id: 10) }
-  #
-  #   specify { expect(subject.war_ids.size).to eq(9) }
-  #
-  #   specify { expect(subject.war_ids.first).to eq(9) }
-  #
-  #   specify { expect(subject.error_limit_remain).to eq(100) }
-  #
-  #   specify { expect(subject.error_limit_reset).to eq(56) }
-  # end
+  context "with max_war_id" do
+    before { VCR.insert_cassette "esi/wars/wars_with_max_war_id" }
+
+    after { VCR.eject_cassette }
+
+    let(:client) { EveOnline::ESI::Client.new }
+
+    subject { client.wars.wars(max_war_id: 10) }
+
+    specify { expect(subject.war_ids.size).to eq(9) }
+
+    specify { expect(subject.war_ids.first).to eq(9) }
+  end
 end
