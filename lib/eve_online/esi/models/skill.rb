d7070ccf88cfc961795a9339fd04c892bb28c759
Igor Zubkov <igor.zubkov@gmail.com>
2026-02-22 19:34:54 +0100

    Update

--- lib/eve_online/esi/models/skill.rb
+++ lib/eve_online/esi/models/skill.rb
@@ -6,6 +6,10 @@ module EveOnline
       class Skill < Object
         def as_json
           {
+            active_skill_level: attributes.active_skill_level,
+            skill_id: attributes.skill_id,
+            skillpoints_in_skill: attributes.skillpoints_in_skill,
+            trained_skill_level: attributes.trained_skill_level
           }
         end
       end
