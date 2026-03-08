9f5baba1a861de4e9f37b2726c45883e71ad0903
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-08 20:06:14 +0100

    Cleanup

--- lib/eve_online/esi/resources/assets_resources.rb
+++ lib/eve_online/esi/resources/assets_resources.rb
@@ -7,7 +7,10 @@ module EveOnline
         # @param id [Integer] The ID of the character.
         # @param page [Integer] Page ID. Default: `1`.
         def character(id:, page: 1)
-          response = get_request("characters/#{id}/assets", params: {page: page})
+          response = get_request("characters/#{id}/assets",
+            params: {
+              page: page
+            })
 
           Collection.from_response(response, type: Models::CharacterAsset)
         end
