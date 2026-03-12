5ff3e8e6090cd1bdc775705eb076c1e5b216097d
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-12 22:02:36 +0100

    Update

--- spec/features/dogma_attributes_spec.rb
+++ spec/features/dogma_attributes_spec.rb
@@ -7,15 +7,29 @@ RSpec.describe "Get attributes" do
 
   after { VCR.eject_cassette }
 
-  subject { EveOnline::ESI::DogmaAttributes.new }
+  let(:client) { EveOnline::ESI::Client.new }
 
-  specify { expect(subject.scope).to eq(nil) }
+  subject { client.dogma.attributes }
 
-  specify { expect(subject.attribute_ids.size).to eq(2463) }
+  specify { expect(subject.attribute_ids.size).to eq(2_825) }
 
-  specify { expect(subject.attribute_ids.first).to eq(2) }
+  specify { expect(subject.attribute_ids.first).to eq(1_413) }
+
+  specify { expect(subject.etag).to eq("W/\"eb02b32cba3ed2b28639d4d552243949671a18d2b8aca54f4271cfbb\"") }
+
+  specify { expect(subject.cache_status).to eq("HIT") }
+
+  specify { expect(subject.request_id).to eq("9744cc2e-f955-47d3-b9a5-4b3aef360988") }
+
+  specify { expect(subject.ratelimit_group).to eq(nil) }
+
+  specify { expect(subject.ratelimit_limit).to eq(nil) }
+
+  specify { expect(subject.ratelimit_remaining).to eq(nil) }
+
+  specify { expect(subject.ratelimit_used).to eq(nil) }
 
   specify { expect(subject.error_limit_remain).to eq(100) }
 
-  specify { expect(subject.error_limit_reset).to eq(37) }
+  specify { expect(subject.error_limit_reset).to eq(19) }
 end
