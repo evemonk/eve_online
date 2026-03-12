7d7481314bf85dc3651c5f083e6307b3340a4b48
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-12 15:25:07 +0100

    Update

--- spec/features/universe_star_spec.rb
+++ spec/features/universe_star_spec.rb
@@ -7,24 +7,37 @@ RSpec.describe "Get star information" do
 
   after { VCR.eject_cassette }
 
-  let(:options) { {id: 40_000_001} }
+  let(:client) { EveOnline::ESI::Client.new }
 
-  subject { EveOnline::ESI::UniverseStar.new(options) }
-
-  specify { expect(subject.scope).to eq(nil) }
+  subject { client.universe.star(id: 40_000_001) }
 
   specify do
-    expect(subject.as_json).to eq(age: 14_262_808_228,
+    expect(subject.as_json).to eq(
+      age: 14_262_808_228,
       luminosity: 0.01575000025331974,
       name: "Tanoo - Star",
-      radius: 126_700_000,
+      radius: 63_350_000,
       solar_system_id: 30_000_001,
       spectral_class: "K2 V",
       temperature: 4567,
       type_id: 45_041)
   end
 
+  specify { expect(subject.etag).to eq("W/\"eb02b32cba3ed2b28639d4d552243949671a18d2b8aca54f4271cfbb\"") }
+
+  specify { expect(subject.cache_status).to eq("HIT") }
+
+  specify { expect(subject.request_id).to eq("560257e5-5235-499c-9731-802f192b336d") }
+
+  specify { expect(subject.ratelimit_group).to eq(nil) }
+
+  specify { expect(subject.ratelimit_limit).to eq(nil) }
+
+  specify { expect(subject.ratelimit_remaining).to eq(nil) }
+
+  specify { expect(subject.ratelimit_used).to eq(nil) }
+
   specify { expect(subject.error_limit_remain).to eq(100) }
 
-  specify { expect(subject.error_limit_reset).to eq(32) }
+  specify { expect(subject.error_limit_reset).to eq(2) }
 end
