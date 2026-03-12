ec8351ed0f769ac636e8776089a6df268eba7235
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-12 15:27:46 +0100

    Update

--- lib/eve_online/esi/models/region.rb
+++ lib/eve_online/esi/models/region.rb
@@ -4,6 +4,13 @@ module EveOnline
   module ESI
     module Models
       class Region < Object
+        def as_json
+          {
+            description: attributes.description,
+            name: attributes.name,
+            region_id: attributes.region_id
+          }
+        end
       end
     end
   end
