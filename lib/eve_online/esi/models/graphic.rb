dd6e28c6d5981175a8996c9bd0ea0bf1a85e96b4
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-12 20:17:57 +0100

    Update

--- lib/eve_online/esi/models/graphic.rb
+++ lib/eve_online/esi/models/graphic.rb
@@ -5,7 +5,16 @@ module EveOnline
     module Models
       class Graphic < Object
         def as_json
-          {}
+          {
+            collision_file: attributes.collision_file,
+            graphic_file: attributes.graphic_file,
+            graphic_id: attributes.graphic_id,
+            icon_folder: attributes.icon_folder,
+            sof_dna: attributes.sof_dna,
+            sof_fation_name: attributes.sof_fation_name,
+            sof_hull_name: attributes.sof_hull_name,
+            sof_race_name: attributes.sof_race_name
+          }
         end
       end
     end
