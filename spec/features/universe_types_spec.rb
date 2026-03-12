ae749dc09a3a43dc75125d2461855090d878ee28
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-12 19:59:53 +0100

    Update

--- spec/features/universe_types_spec.rb
+++ spec/features/universe_types_spec.rb
@@ -7,19 +7,31 @@ RSpec.describe "Get types" do
 
   after { VCR.eject_cassette }
 
-  subject { EveOnline::ESI::UniverseTypes.new }
+  let(:client) { EveOnline::ESI::Client.new }
 
-  specify { expect(subject.scope).to eq(nil) }
+  subject { client.universe.types }
 
-  specify { expect(subject.page).to eq(1) }
-
-  specify { expect(subject.total_pages).to eq(36) }
+  specify { expect(subject.total_pages).to eq(52) }
 
   specify { expect(subject.universe_type_ids.size).to eq(1000) }
 
   specify { expect(subject.universe_type_ids.first).to eq(0) }
 
+  specify { expect(subject.etag).to eq("W/\"eb02b32cba3ed2b28639d4d552243949671a18d2b8aca54f4271cfbb\"") }
+
+  specify { expect(subject.cache_status).to eq("HIT") }
+
+  specify { expect(subject.request_id).to eq("f5830882-1403-48e4-9a49-66c26dcd1ac9") }
+
+  specify { expect(subject.ratelimit_group).to eq(nil) }
+
+  specify { expect(subject.ratelimit_limit).to eq(nil) }
+
+  specify { expect(subject.ratelimit_remaining).to eq(nil) }
+
+  specify { expect(subject.ratelimit_used).to eq(nil) }
+
   specify { expect(subject.error_limit_remain).to eq(100) }
 
-  specify { expect(subject.error_limit_reset).to eq(45) }
+  specify { expect(subject.error_limit_reset).to eq(6) }
 end
