da7cdd0561f3248ca1debf432e9eaf7a89fbb43d
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-12 15:03:19 +0100

    Update

--- lib/eve_online/esi/resources/universe_resources.rb
+++ lib/eve_online/esi/resources/universe_resources.rb
@@ -64,21 +64,29 @@ module EveOnline
 
         def regions
           response = get_request("universe/regions")
+
+          Models::Regions.new(body: response.body, headers: response.headers)
         end
 
         # @param id [Integer] Region ID
         def region(id:)
           response = get_request("universe/regions/#{id}")
+
+          Models::Region.new(attributes: response.body, headers: response.headers)
         end
 
         # @param id [Integer] Star ID
         def star(id:)
           response = get_request("universe/stars/#{id}")
+
+          Models::Star.new(attributes: response.body, headers: response.headers)
         end
 
         # @param id [Integer] Stargate ID
         def stargate(id:)
           response = get_request("universe/stargates/#{id}")
+
+          Models::Stargate.new(attributes: response.body, headers: response.headers)
         end
 
         private
--- spec/features/universe_regions_spec.rb
+++ spec/features/universe_regions_spec.rb
@@ -7,15 +7,29 @@ RSpec.describe "Get regions" do
 
   after { VCR.eject_cassette }
 
-  subject { EveOnline::ESI::UniverseRegions.new }
+  let(:client) { EveOnline::ESI::Client.new }
 
-  specify { expect(subject.scope).to eq(nil) }
+  subject { client.universe.regions }
 
-  specify { expect(subject.universe_region_ids.size).to eq(106) }
+  specify { expect(subject.region_ids.size).to eq(114) }
 
-  specify { expect(subject.universe_region_ids.first).to eq(10_000_001) }
+  specify { expect(subject.region_ids.first).to eq(10_000_001) }
+
+  specify { expect(subject.etag).to eq("W/\"eb02b32cba3ed2b28639d4d552243949671a18d2b8aca54f4271cfbb\"") }
+
+  specify { expect(subject.cache_status).to eq("HIT") }
+
+  specify { expect(subject.request_id).to eq("e78de276-27a0-4592-9e62-be231990f233") }
+
+  specify { expect(subject.ratelimit_group).to eq(nil) }
+
+  specify { expect(subject.ratelimit_limit).to eq(nil) }
+
+  specify { expect(subject.ratelimit_remaining).to eq(nil) }
+
+  specify { expect(subject.ratelimit_used).to eq(nil) }
 
   specify { expect(subject.error_limit_remain).to eq(100) }
 
-  specify { expect(subject.error_limit_reset).to eq(59) }
+  specify { expect(subject.error_limit_reset).to eq(36) }
 end
