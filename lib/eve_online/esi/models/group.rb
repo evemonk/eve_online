9f36e3fbfcf7140b84d16bbc5fac6a08840eb17e
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-12 17:31:53 +0100

    Update

--- lib/eve_online/esi/models/group.rb
+++ lib/eve_online/esi/models/group.rb
@@ -6,8 +6,15 @@ module EveOnline
       class Group < Object
         def as_json
           {
-
+            category_id: attributes.category_id,
+            group_id: attributes.group_id,
+            name: attributes.name,
+            published: attributes.published
           }
+        end
+
+        def type_ids
+          attributes.types || []
         end
       end
     end
--- spec/features/universe_group_spec.rb
+++ spec/features/universe_group_spec.rb
@@ -7,11 +7,9 @@ RSpec.describe "Get item group information" do
 
   after { VCR.eject_cassette }
 
-  let(:options) { {id: 450, language: "en-us"} }
+  let(:client) { EveOnline::ESI::Client.new }
 
-  subject { EveOnline::ESI::UniverseGroup.new(options) }
-
-  specify { expect(subject.scope).to eq(nil) }
+  subject { client.universe.group(id: 450) }
 
   specify do
     expect(subject.as_json).to eq(category_id: 25,
@@ -20,11 +18,25 @@ RSpec.describe "Get item group information" do
       published: true)
   end
 
-  specify { expect(subject.type_ids.size).to eq(10) }
+  specify { expect(subject.type_ids.size).to eq(14) }
 
   specify { expect(subject.type_ids.first).to eq(22) }
 
+  specify { expect(subject.etag).to eq("W/\"eb02b32cba3ed2b28639d4d552243949671a18d2b8aca54f4271cfbb\"") }
+
+  specify { expect(subject.cache_status).to eq("HIT") }
+
+  specify { expect(subject.request_id).to eq("d6a53435-7ac4-4c08-b845-178f29ead84d") }
+
+  specify { expect(subject.ratelimit_group).to eq(nil) }
+
+  specify { expect(subject.ratelimit_limit).to eq(nil) }
+
+  specify { expect(subject.ratelimit_remaining).to eq(nil) }
+
+  specify { expect(subject.ratelimit_used).to eq(nil) }
+
   specify { expect(subject.error_limit_remain).to eq(100) }
 
-  specify { expect(subject.error_limit_reset).to eq(26) }
+  specify { expect(subject.error_limit_reset).to eq(21) }
 end
