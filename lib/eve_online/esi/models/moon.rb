512647aa712eea22b9f28c64445c0159270c986e
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-12 14:13:57 +0100

    Update

--- lib/eve_online/esi/models/moon.rb
+++ lib/eve_online/esi/models/moon.rb
@@ -6,7 +6,14 @@ module EveOnline
       class Moon < Object
         def as_json
           {
+            moon_id: attributes.moon_id,
+            name: attributes.name,
+            system_id: attributes.system_id
           }
+        end
+
+        def position
+          @position ||= Position.new(attributes: attributes.position)
         end
       end
     end
