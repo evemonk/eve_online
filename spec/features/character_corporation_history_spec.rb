12a9b9129b50c3a344aa4b14807e70fa91c36e8c
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-14 15:09:50 +0100

    Update

--- spec/features/character_corporation_history_spec.rb
+++ spec/features/character_corporation_history_spec.rb
@@ -3,26 +3,38 @@
 require "spec_helper"
 
 RSpec.describe "Get corporation history" do
-  let(:options) { {character_id: 90_729_314} }
-
-  before { VCR.insert_cassette "esi/character_corporation_history/90729314" }
+  before { VCR.insert_cassette "esi/character_corporation_history/1337512245" }
 
   after { VCR.eject_cassette }
 
-  subject { EveOnline::ESI::CharacterCorporationHistory.new(options) }
+  let(:client) { EveOnline::ESI::Client.new }
 
-  specify { expect(subject.scope).to eq(nil) }
+  subject { client.characters.corporation_history(id: 1_337_512_245) }
 
-  specify { expect(subject.entries.size).to eq(1) }
+  specify { expect(subject.entries.size).to eq(11) }
 
   specify do
-    expect(subject.entries.first.as_json).to eq(corporation_id: 1_000_168,
+    expect(subject.entries.first.as_json).to eq(corporation_id: 1_000_171,
       is_deleted: nil,
-      record_id: 16_785_803,
-      start_date: "2011-05-10T10:23:00Z")
+      record_id: 64_489_559,
+      start_date: Time.utc(2025, 4, 24, 19, 2, 0))
   end
+
+  specify { expect(subject.etag).to eq("\"2101d3bfd27c5a53d0b90b07761e5716010bb393493110c3266cd730\"") }
+
+  specify { expect(subject.cache_status).to eq("HIT") }
+
+  specify { expect(subject.request_id).to eq("3aff0c93-867f-4671-9cd5-bfec95b4bf43") }
+
+  specify { expect(subject.ratelimit_group).to eq(nil) }
+
+  specify { expect(subject.ratelimit_limit).to eq(nil) }
+
+  specify { expect(subject.ratelimit_remaining).to eq(nil) }
+
+  specify { expect(subject.ratelimit_used).to eq(nil) }
 
   specify { expect(subject.error_limit_remain).to eq(100) }
 
-  specify { expect(subject.error_limit_reset).to eq(17) }
+  specify { expect(subject.error_limit_reset).to eq(41) }
 end
