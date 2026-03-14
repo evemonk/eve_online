ca8dec644136fce7cc6108f1482d0025c5422fa5
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-14 13:58:36 +0100

    Update

--- lib/eve_online/esi/models/dogma_effect_modifier.rb
+++ lib/eve_online/esi/models/dogma_effect_modifier.rb
@@ -5,7 +5,14 @@ module EveOnline
     module Models
       class DogmaEffectModifier < Object
         def as_json
-          {}
+          {
+            domain: attributes.domain,
+            effect_id: attributes.effect_id,
+            func: attributes.func,
+            modified_attribute_id: attributes.modified_attribute_id,
+            modifying_attribute_id: attributes.modifying_attribute_id,
+            operator: attributes.operator
+          }
         end
       end
     end
--- spec/features/dogma_effect_spec.rb
+++ spec/features/dogma_effect_spec.rb
@@ -34,6 +34,17 @@ RSpec.describe "Get effect information" do
       tracking_speed_attribute_id: nil)
   end
 
+  specify { expect(subject.modifiers.size).to eq(4) }
+
+  specify do
+    expect(subject.modifiers.first.as_json).to eq(domain: "shipID",
+      effect_id: nil,
+      func: "LocationRequiredSkillModifier",
+      modified_attribute_id: 73,
+      modifying_attribute_id: 2_458,
+      operator: 6)
+  end
+
   specify { expect(subject.etag).to eq("W/\"eb02b32cba3ed2b28639d4d552243949671a18d2b8aca54f4271cfbb\"") }
 
   specify { expect(subject.cache_status).to eq("HIT") }
