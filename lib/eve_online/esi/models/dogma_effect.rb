6a5e9eca54b5b27b88565f9b3c8f3394b5cab556
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-14 13:22:21 +0100

    Update

--- lib/eve_online/esi/models/dogma_effect.rb
+++ lib/eve_online/esi/models/dogma_effect.rb
@@ -5,7 +5,28 @@ module EveOnline
     module Models
       class DogmaEffect < Object
         def as_json
-          {}
+          {
+            description: attributes.description,
+            disallow_auto_repeat: attributes.disallow_auto_repeat,
+            discharge_attribute_id: attributes.discharge_attribute_id,
+            display_name: attributes.display_name,
+            duration_attribute_id: attributes.duration_attribute_id,
+            effect_category: attributes.effect_category,
+            effect_id: attributes.effect_id,
+            electronic_chance: attributes.electronic_chance,
+            falloff_attribute_id: attributes.falloff_attribute_id,
+            icon_id: attributes.icon_id,
+            is_assistance: attributes.is_assistance,
+            is_offensive: attributes.is_offensive,
+            is_warp_safe: attributes.is_warp_safe,
+            name: attributes.name,
+            post_expression: attributes.post_expression,
+            pre_expression: attributes.pre_expression,
+            published: attributes.published,
+            range_attribute_id: attributes.range_attribute_id,
+            range_chance: attributes.range_chance,
+            tracking_speed_attribute_id: tracking_speed_attribute_id
+          }
         end
       end
     end
--- spec/features/dogma_effect_spec.rb
+++ spec/features/dogma_effect_spec.rb
@@ -7,11 +7,9 @@ RSpec.describe "Get effect information" do
 
   after { VCR.eject_cassette }
 
-  let(:options) { {id: 6717} }
+  let(:client) { EveOnline::ESI::Client.new }
 
-  subject { EveOnline::ESI::DogmaEffect.new(options) }
-
-  specify { expect(subject.scope).to eq(nil) }
+  subject { client.dogma.effect(id: 6_717) }
 
   specify do
     expect(subject.as_json).to eq(description: "Automatically generated effect",
@@ -28,15 +26,29 @@ RSpec.describe "Get effect information" do
       is_offensive: nil,
       is_warp_safe: nil,
       name: "roleBonusIceOreMiningDurationCap",
-      post_expression: 19_291,
-      pre_expression: 19_290,
+      post_expression: nil,
+      pre_expression: nil,
       published: nil,
       range_attribute_id: nil,
       range_chance: nil,
       tracking_speed_attribute_id: nil)
   end
 
+  specify { expect(subject.etag).to eq("W/\"eb02b32cba3ed2b28639d4d552243949671a18d2b8aca54f4271cfbb\"") }
+
+  specify { expect(subject.cache_status).to eq("HIT") }
+
+  specify { expect(subject.request_id).to eq("186c1667-ac1e-4b70-ba58-d732fe508345") }
+
+  specify { expect(subject.ratelimit_group).to eq(nil) }
+
+  specify { expect(subject.ratelimit_limit).to eq(nil) }
+
+  specify { expect(subject.ratelimit_remaining).to eq(nil) }
+
+  specify { expect(subject.ratelimit_used).to eq(nil) }
+
   specify { expect(subject.error_limit_remain).to eq(100) }
 
-  specify { expect(subject.error_limit_reset).to eq(28) }
+  specify { expect(subject.error_limit_reset).to eq(41) }
 end
