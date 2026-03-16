66865763da513884644bac3af3fefb622bfebd18
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-16 17:07:25 +0100

    Update

--- lib/eve_online.rb
+++ lib/eve_online.rb
@@ -28,6 +28,7 @@ module EveOnline
       autoload :CorporationsResources, "eve_online/esi/resources/corporations_resources"
       autoload :DogmaResources, "eve_online/esi/resources/dogma_resources"
       autoload :FactionWarfareResources, "eve_online/esi/resources/faction_warfare_resources"
+      autoload :FittingsResources, "eve_online/esi/resources/fittings_resources"
       autoload :LocationResources, "eve_online/esi/resources/location_resources"
       autoload :LoyaltyPointsResources, "eve_online/esi/resources/loyalty_points_resources"
       autoload :RoutesResources, "eve_online/esi/resources/routes_resources"
